f22cb785512c059ed9572b49acda97cda5bce236
williamwutq <williamwutq@gmail.com>
2025-03-19 14:50:29 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main file of the game
+ * Run main
+ *
+ */
+
+public class Main{
+    public static void main(String[] args){
+        // The main method should only call method in Game class
+    }
+}
