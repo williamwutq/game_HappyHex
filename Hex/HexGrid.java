6f49462cb052badffeafe7bcd03ade2af938f6ea
williamwutq <williamwutq@gmail.com>
2025-03-19 20:37:57 -0700

    Create HexGrid.java

---
+++ Hex/HexGrid.java
@@ -0,0 +1,20 @@
+package Hex;
+
+import java.awt.*;
+
+interface HexGrid {
+    abstract int length();
+    abstract Block[] blocks();
+    abstract boolean inRange(int i, int k);
+    abstract Block getBlock(int i, int k);
+    default boolean getState(int i, int k){
+        return getBlock(i, k).getState();
+    }
+    default Color getColor(int i, int k){
+        return getBlock(i, k).color();
+    }
+    abstract void add(Block origin, HexGrid other);
+    default void add(HexGrid other){
+        add(new Block(), other);
+    }
+}
