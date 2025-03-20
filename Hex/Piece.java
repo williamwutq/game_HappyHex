158c10f7ceeab98435a2487d673fede7423e19a7
williamwutq <williamwutq@gmail.com>
2025-03-20 08:51:50 -0700

    Create Piece.java and basic logics

---
+++ Hex/Piece.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+class Piece implements HexGrid{
+    private Block[] blocks;
+    public Piece(){
+        // To be implemented
+    }
+
+    // Implements HexGrid
+    public int length(){
+        if(blocks == null){
+            return 0;
+        }else{
+            return blocks.length;
+        }
+    }
+    public Blocks[] blocks(){
+        return this.blocks;
+    }
+    public boolean inRange(int i, int k){
+        return true; // Placeholder
+    }
+    public Block getBlock(int i, int k){
+        if(inRange(i, k)){
+            // Implement logic
+            return blocks[0]; // Placeholder
+        }else{
+            return null;
+        }
+    }
+    public void add(Block origin, HexGrid other){
+        // To be implemented
+    }
+}
