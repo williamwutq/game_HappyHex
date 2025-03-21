e9585e2484d54d311a771dec3bdf639ddffe86b5
williamwutq <williamwutq@gmail.com>
2025-03-20 18:58:12 -0700

    Added indexed getBlock and static pieces into Piece.java

--- Hex/Piece.java
+++ Hex/Piece.java
@@ -1,8 +1,9 @@
 package Hex;
 
+import GUI.GameEssentials;
 import java.awt.*;
 
-class Piece implements HexGrid{
+public class Piece implements HexGrid{
     private Block[] blocks;
     private Color color;
     public Piece(){
@@ -89,6 +90,96 @@ class Piece implements HexGrid{
         throw new IllegalArgumentException("Adding Grid to piece prohibited. Please add block by block.");
     }
 
+    // Static pieces
+    public static Piece bigBlock() {
+        Piece p = new Piece(7, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(0,1));
+        p.add(Block.block(1,0));
+        p.add(Block.block(1,1));
+        p.add(Block.block(1,2));
+        p.add(Block.block(2,1));
+        p.add(Block.block(2,2));
+        return p;
+    }
+    public static Piece triangle3A() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(0,1));
+        p.add(Block.block(1,1));
+        return p;
+    }
+    public static Piece triangle3B() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(1,0));
+        p.add(Block.block(1,1));
+        return p;
+    }
+    public static Piece line3I() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(0,1));
+        p.add(Block.block(0,2));
+        return p;
+    }
+    public static Piece line3J() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(1,1));
+        p.add(Block.block(2,2));
+        return p;
+    }
+    public static Piece line3K() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,0));
+        p.add(Block.block(1,0));
+        p.add(Block.block(2,0));
+        return p;
+    }
+    public static Piece corner3I() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(-1,0));
+        p.add(Block.block(0,0));
+        p.add(Block.block(1,1));
+        return p;
+    }
+    public static Piece corner3J() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(0,1));
+        p.add(Block.block(0,0));
+        p.add(Block.block(1,0));
+        return p;
+    }
+    public static Piece corner3K() {
+        Piece p = new Piece(3, GameEssentials.generateColor());
+        p.add(Block.block(1,1));
+        p.add(Block.block(0,0));
+        p.add(Block.block(0,-1));
+        return p;
+    }
+    public static Piece generatePiece(){
+        return switch((int)(Math.random() * 19)){
+            case 1, 2, 3 -> triangle3A();
+            case 4, 5, 6 -> triangle3B();
+            case 7, 8 -> line3I();
+            case 9, 10 -> line3J();
+            case 11, 12 -> line3K();
+            case 13, 14 -> corner3I();
+            case 15, 16 -> corner3J();
+            case 17, 18 -> corner3K();
+            default -> bigBlock();
+        };
+    }
+
+    public String toString(){
+        StringBuilder str = new StringBuilder("{Piece: ");
+        for (Block block : blocks) {
+            str.append(block.getLines());
+        }
+        return str + "}";
+    }
+
     public static void main(String[] args){
         Piece p1 = new Piece(5, Color.BLUE);
         System.out.println(p1.add(Block.block(0,0)));// Should be true
@@ -103,5 +194,10 @@ class Piece implements HexGrid{
         System.out.println(p1.inRange(1,1));// Should be true
         System.out.println();
         System.out.println(p1.getBlock(0,1));// Should have Color (0, 0, 255)
+        System.out.println();
+        System.out.println(p1); // Print out this
+        System.out.println(generatePiece()); // Print out random
+        System.out.println(generatePiece()); // Print out random
+        System.out.println(generatePiece()); // Print out random
     }
 }
