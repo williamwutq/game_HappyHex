e4177da41c5917af964f1356fc5f527ecff3b6ea
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-21 11:21:59 -0700

    Create GamePanel.java

---
+++ GUI/GamePanel.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import Hex.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel{
+    public GamePanel(HexEngine engine){
+        super();
+        // Size should be dynamic
+    }
+}
