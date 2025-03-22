ed48ad5a6d24d526b75f1430407641ce5737800d
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-21 18:19:16 -0700

    Added positioning and size to make CircularButton work

--- GUI/CircularButton.java
+++ GUI/CircularButton.java
@@ -2,6 +2,7 @@ package GUI;
 
 import Hex.Hex;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -10,15 +11,38 @@ public class CircularButton extends JButton implements ActionListener {
         super();
         this.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.setAlignmentY(Component.CENTER_ALIGNMENT);
+        this.setForeground(Color.BLACK);
+        this.setBackground(new Color(0,0,0,0));
+        this.setBorder(new EmptyBorder(0,0,0,0));
         this.addActionListener(this);
     }
+    public void setSize(int size){
+        Dimension dimension = new Dimension(size, size);
+        this.setSize(dimension);
+        this.setMinimumSize(dimension);
+        this.setMaximumSize(dimension);
+        this.setPreferredSize(dimension);
+    }
+    public void setPosition(int x, int y){
+        this.setBounds(x - this.getWidth()/2,y - this.getHeight()/2, this.getWidth(), this.getHeight());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(this.getForeground());
+        g.fillOval(0,0,this.getWidth(), this.getHeight());
+    }
+
     public void actionPerformed(ActionEvent e){
         // When it is clicked do something
     }
     public static void main(String[] args){
         JFrame frame = new JFrame("Test: CircularButton");
         JPanel panel = new JPanel();
-        JButton circularbutton = new CircularButton(new Hex(), null);
+        CircularButton circularbutton = new CircularButton(new Hex(), null);
+        circularbutton.setSize(50);
+        circularbutton.setPosition(30, 60);
         panel.setLayout(null);
         panel.add(circularbutton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
