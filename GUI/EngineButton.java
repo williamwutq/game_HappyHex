ad8e7356ac04f7164a6b8e5abc1d602d65f298bc
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 12:10:39 -0700

    Added correct fetch height extension in EngineButton.java as radius of game engine - 1

--- GUI/EngineButton.java
+++ GUI/EngineButton.java
@@ -7,6 +7,9 @@ public class EngineButton extends HexButton {
     public EngineButton(int index){
         super(index);
     }
+    protected int fetchHeightExtension(){
+        return GameEssentials.engine().getRadius() - 1;
+    }
     protected Block fetchBlock(){
         return GameEssentials.engine().getBlock(getIndex());
     }
@@ -21,10 +24,12 @@ public class EngineButton extends HexButton {
     }
 
     public void clicked(){
+        GameEssentials.turn ++;
         // Fetch position
         Hex position = fetchBlock().thisHex();
         // Check this position, if good then add
         if(GameEssentials.engine().checkAdd(position, GameEssentials.queue().getFirst())){
+            GameEssentials.score += GameEssentials.queue().getFirst().length();
             GameEssentials.engine().add(position, GameEssentials.queue().next());
         } else {
             // For now: check queue
@@ -36,7 +41,12 @@ public class EngineButton extends HexButton {
             // Eliminate code is here, see GameTimer class
             new GameTimer().start();
         } else if (GamePanel.checkEnd()){
+            System.out.println("---------- Game Over ----------");
+            System.out.println("This game lasted for " + GameEssentials.turn + " turns.");
+            System.out.println("The total score is " + GameEssentials.score + " points.");
             // Reset
+            GameEssentials.score = 0;
+            GameEssentials.turn = 0;
             GameEssentials.engine().reset();
             GameEssentials.window().repaint();
         }
@@ -45,6 +55,7 @@ public class EngineButton extends HexButton {
         Hex position = fetchBlock().thisHex();
         for(int i = 0; i < GameEssentials.queue().length(); i ++){
             if(GameEssentials.engine().checkAdd(position, GameEssentials.queue().get(i))){
+                GameEssentials.score += GameEssentials.queue().get(i).length();
                 GameEssentials.engine().add(position, GameEssentials.queue().fetch(i));
                 break;
             }
