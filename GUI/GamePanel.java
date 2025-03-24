998ceb14383e7b66151a4fda685c16c19b017bf3
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 00:45:17 -0700

    Deleted unnecessary methods in GamePanel.java

--- GUI/GamePanel.java
+++ GUI/GamePanel.java
@@ -30,70 +30,6 @@ public class GamePanel extends JPanel {
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
         super.paintChildren(g);
     }
-    // Test: autoplay
-    public static void autoplay(int size, int queueSize, int delay){
-        // Frame
-        JFrame frame = new JFrame("Test: GamePanel");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setBackground(Color.WHITE);
-        frame.setAlwaysOnTop(true);
-        frame.setSize(new Dimension(800, 800));
-        frame.setMinimumSize(new Dimension(200, 200));
-
-        // Engine
-        HexEngine engine = new HexEngine(size);
-        Queue queue = new Queue(queueSize);
-        GameEssentials.setEngine(engine);
-        GameEssentials.setQueue(queue);
-        GameEssentials.setWindow(frame);
-        GamePanel gamePanel = new GamePanel();
-        frame.add(gamePanel, BorderLayout.CENTER);
-        frame.setVisible(true);
-        // Autoplay
-        int turns = 0;
-        int score = 0;
-        while (true) {
-            Piece piece = queue.getFirst();
-            // Check positions
-            try {
-                engine.add(engine.checkPositions(piece).get((int) (Math.random() * engine.checkPositions(piece).size())), piece);
-                queue.next();
-            } catch (IllegalArgumentException | IndexOutOfBoundsException e) {
-                int index = 1;
-                while(index < queueSize) {
-                    piece = queue.get(index);
-                    try {
-                        engine.add(engine.checkPositions(piece).get((int) (Math.random() * engine.checkPositions(piece).size())), piece);
-                        queue.fetch(index);
-                        break;
-                    } catch (IllegalArgumentException | IndexOutOfBoundsException ex) {
-                        index ++;
-                    }
-                }
-                if(index >= queueSize){
-                    // No possible solutions
-                    break;
-                }
-            }
-            score += piece.length();
-            gamePanel.repaint();
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {}
-            score += engine.eliminate() * 3;
-            gamePanel.repaint();
-            turns++;
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {}
-        }
-        engine.reset();
-        gamePanel.repaint();
-        System.out.println("Auto Play: Game Over");
-        System.out.println("This game lasted for " + turns + " turns.");
-        System.out.println("The total score is " + score + " points.");
-    }
     public static void play (int size, int queueSize, int delay) {
         // Frame
         JFrame frame = new JFrame("Test: GamePanel");
@@ -110,14 +46,9 @@ public class GamePanel extends JPanel {
         GameEssentials.setEngine(engine);
         GameEssentials.setQueue(queue);
         GameEssentials.setWindow(frame);
+        GameEssentials.setDelay(delay);
         GamePanel gamePanel = new GamePanel();
-        //PiecePanel piecePanel = new PiecePanel();
         frame.add(gamePanel, BorderLayout.CENTER);
-        //frame.add(piecePanel, BorderLayout.SOUTH);
         frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        play(9, 9, 200);
     }
 }
