9afea19e17799ac2556823d852b717d850010c12
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 00:30:19 -0700

    Create GameTimer.java

---
+++ GUI/GameTimer.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GameTimer extends Timer implements ActionListener {
+    public GameTimer() {
+        super(GameEssentials.getDelay(), null);
+        this.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        GameEssentials.engine().eliminate();
+        GameEssentials.window().repaint();
+    }
+}
