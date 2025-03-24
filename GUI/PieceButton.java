9679fc5b08f575294d5ff738a0db608d0a7a069e
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 13:10:47 -0700

    Added correct extension length to PieceButton.java

--- GUI/PieceButton.java
+++ GUI/PieceButton.java
@@ -4,14 +4,42 @@ import Hex.*;
 import java.awt.Color;
 
 public class PieceButton extends HexButton {
-    public PieceButton(int index){
-        super(index);
+    private int pieceIndex;
+    public PieceButton(int pieceIndex, int blockIndex){
+        super(blockIndex);
+        this.pieceIndex = pieceIndex;
     }
+
+    protected int fetchHeightExtension() {
+        // Depend on block, searching for min distance to J
+        Piece piece = GameEssentials.queue().get(pieceIndex);
+        int minPosition = 0;
+        for(int i = 0; i < piece.length(); i ++){
+            int currentPosition = piece.getBlock(i).getLineJ();
+            if(currentPosition < minPosition){
+                minPosition = currentPosition;
+            }
+        }
+        return -minPosition;
+    }
+    protected int fetchWidthExtension() {
+        // Depend on block, searching for min J value
+        Piece piece = GameEssentials.queue().get(pieceIndex);
+        int minPosition = 0;
+        for(int i = 0; i < piece.length(); i ++){
+            int currentPosition = piece.getBlock(i).J();
+            if(currentPosition < minPosition){
+                minPosition = currentPosition;
+            }
+        }
+        return -minPosition;
+    }
+
     protected Block fetchBlock() {
-        return GameEssentials.queue().getFirst().getBlock(getIndex());
+        return GameEssentials.queue().get(pieceIndex).getBlock(getIndex());
     }
     protected Color fetchColor() {
-        return GameEssentials.queue().getFirst().getColor();
+        return GameEssentials.queue().get(pieceIndex).getColor();
     }
     protected void clicked() {
         // Do nothing currently
