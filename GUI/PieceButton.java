e5b5930b204a453b91aa4a53113ddd24bc885229
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 22:15:42 -0700

    Update PieceButton.java to use simpler extension logic as pieces are now in a grid

--- GUI/PieceButton.java
+++ GUI/PieceButton.java
@@ -4,35 +4,18 @@ import Hex.*;
 import java.awt.Color;
 
 public class PieceButton extends HexButton {
-    private int pieceIndex;
+    private final int pieceIndex;
     public PieceButton(int pieceIndex, int blockIndex){
         super(blockIndex);
         this.pieceIndex = pieceIndex;
+        super.resetSize();
     }
 
     protected int fetchHeightExtension() {
-        // Depend on block, searching for min distance to J
-        Piece piece = GameEssentials.queue().get(pieceIndex);
-        int minPosition = 0;
-        for(int i = 0; i < piece.length(); i ++){
-            int currentPosition = piece.getBlock(i).getLineJ();
-            if(currentPosition < minPosition){
-                minPosition = currentPosition;
-            }
-        }
-        return -minPosition;
+        return 1;
     }
     protected int fetchWidthExtension() {
-        // Depend on block, searching for min J value
-        Piece piece = GameEssentials.queue().get(pieceIndex);
-        int minPosition = 0;
-        for(int i = 0; i < piece.length(); i ++){
-            int currentPosition = piece.getBlock(i).J();
-            if(currentPosition < minPosition){
-                minPosition = currentPosition;
-            }
-        }
-        return -minPosition;
+        return 2 + 6 * pieceIndex;
     }
 
     protected Block fetchBlock() {
