4421b552cec240c12da06d26adbf049c4870e335
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-24 22:19:26 -0700

    Changed PiecePanel.java and added test cases

--- GUI/PiecePanel.java
+++ GUI/PiecePanel.java
@@ -1,6 +1,8 @@
 package GUI;
 
-import Hex.*;
+import Hex.HexEngine;
+import Hex.Queue;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,22 +13,38 @@ public class PiecePanel extends JPanel{
         this.setBackground(Color.GRAY);
         // Basic graphics
         this.setLayout(null); // Use absolute
-        int width = (int) Math.round(20 * HexButton.getActiveSize() * GameEssentials.sinOf60);
-        int height = (int) Math.round(10 * HexButton.getActiveSize());
+        int width = (int) Math.round(10 * HexButton.getActiveSize() * GameEssentials.sinOf60);
+        int height = (int) Math.round(5 * HexButton.getActiveSize());
         this.setPreferredSize(new Dimension(width, height));
-        // Prepare
-        HexButton.setSize(1);
         // Construct buttons
-        for (int i = 0; i < GameEssentials.queue().getFirst().length(); i++) {
-            //this.add(new PieceButton(i));
+        for(int p = 0; p < GameEssentials.queue().length(); p ++) {
+            for (int i = 0; i < GameEssentials.queue().get(p).length(); i++) {
+                this.add(new PieceButton(p, i));
+            }
         }
     }
     public void paint(java.awt.Graphics g) {
-        int width = (int) Math.round(20 * HexButton.getActiveSize() * GameEssentials.sinOf60);
-        int height = (int) Math.round(10 * HexButton.getActiveSize());
+        int width = (int) Math.round(10 * HexButton.getActiveSize() * GameEssentials.sinOf60);
+        int height = (int) Math.round(5 * HexButton.getActiveSize());
         this.setPreferredSize(new Dimension(width, height));
         g.setColor(this.getBackground());
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
         super.paintChildren(g);
+    }
+    public static void main(String[] args){
+        // Frame
+        JFrame frame = new JFrame("Test: PiecePanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setBackground(Color.WHITE);
+        frame.setSize(new Dimension(800, 800));
+        frame.setMinimumSize(new Dimension(200, 200));
+
+        HexButton.setSize(50);
+        GameEssentials.setQueue(new Queue(5));
+        GameEssentials.setEngine(new HexEngine(5));
+        PiecePanel piecePanel = new PiecePanel();
+        frame.add(piecePanel, BorderLayout.SOUTH);
+        frame.setVisible(true);
     }
 }
