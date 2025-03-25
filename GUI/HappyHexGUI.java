4be670744c3b24c21ff336ae0470b06fe1c0aefa
William Wu <204092015+williamwutq@users.noreply.github.com>
2025-03-25 01:18:01 -0700

    Added basic play into HappyHexGUI.java

--- GUI/HappyHexGUI.java
+++ GUI/HappyHexGUI.java
@@ -1,5 +1,37 @@
 package GUI;
 
+import Hex.HexEngine;
+import Hex.Queue;
+
+import javax.swing.*;
+import java.awt.*;
+
 class HappyHexGUI{
-    // Implement the main GUI here
+    public static void play(int size, int queueSize, int delay) {
+        // Frame
+        JFrame frame = new JFrame("Test: GamePanel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.setBackground(Color.WHITE);
+        frame.setSize(new Dimension(800, 800));
+        frame.setMinimumSize(new Dimension(200, 200));
+
+        // Engine
+        HexEngine engine = new HexEngine(size);
+        Queue queue = new Queue(queueSize);
+        GameEssentials.setEngine(engine);
+        GameEssentials.setQueue(queue);
+        GameEssentials.setWindow(frame);
+        GameEssentials.setDelay(delay);
+        GameEssentials.calculateButtonSize();
+        GamePanel gamePanel = new GamePanel();
+        PiecePanel piecePanel = new PiecePanel();
+        frame.add(gamePanel, BorderLayout.CENTER);
+        frame.add(piecePanel, BorderLayout.SOUTH);
+        frame.setVisible(true);
+        frame.repaint();
+    }
+    public static void main(String[] args) {
+        play(9, 6, 200);
+    }
 }
