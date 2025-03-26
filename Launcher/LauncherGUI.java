49ee55fa7d8a664301cad09820a9c29a7b7ac4a0
williamwutq <williamwutq@gmail.com>
2025-03-26 09:49:31 -0700

    Created the Launcher package and LauncherGUI.java

---
+++ Launcher/LauncherGUI.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+public class LauncherGUI {
+    public static void launch(){
+        // Launch code there
+    }
+    public static void main(String[] args){
+        launch();
+    }
+}
