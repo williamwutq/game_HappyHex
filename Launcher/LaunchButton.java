9d2ce09835b7fc1db1780f8f28f90003d85d3ec4
williamwutq <williamwutq@gmail.com>
2025-03-26 10:08:28 -0700

    Created LaunchButton.java and LaunchEssentials.java

---
+++ Launcher/LaunchButton.java
@@ -0,0 +1,16 @@
+package Launcher;
+
+import java.awt.*;
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LaunchButton extends JButton implements ActionListener {
+    public LaunchButton(){
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        // When button is clicked
+    }
+}
---
+++ Launcher/LaunchEssentials.java
@@ -0,0 +1,9 @@
+package Launcher;
+
+/**
+ * The {@link LaunchEssentials} class provides essential launcher utilities.
+ * This class is final and cannot be extended.
+ */
+public final class LaunchEssentials {
+
+}
