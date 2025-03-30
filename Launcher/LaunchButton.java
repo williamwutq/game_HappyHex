e17649aa0453402977fd4c5fb7c9173b11dc1049
williamwutq <williamwutq@gmail.com>
2025-03-29 20:27:06 -0700

    Implemented basic graphics in LaunchButton.java

--- Launcher/LaunchButton.java
+++ Launcher/LaunchButton.java
@@ -2,15 +2,25 @@ package Launcher;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class LaunchButton extends JButton implements ActionListener {
     public LaunchButton(){
-
+        this.setText("START");
+        this.setForeground(LaunchEssentials.launchStartButtonFontColor);
+        this.setBackground(LaunchEssentials.launchStartButtonBackgroundColor);
+        this.setOpaque(true);
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.setAlignmentY(Component.CENTER_ALIGNMENT);
+        this.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setVerticalAlignment(SwingConstants.CENTER);
+        this.setFont(new Font(LaunchEssentials.launchStartButtonFont, Font.BOLD, 30));
+        this.setBorder(new EmptyBorder(5,15,5,15));
+        this.addActionListener(this);
     }
     public void actionPerformed(ActionEvent e) {
-        // When button is clicked
+        LaunchEssentials.startGame();
     }
 }
