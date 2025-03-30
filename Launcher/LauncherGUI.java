eadd64fbf0725f945f8ab9d514f23bfc3c3ac889
williamwutq <williamwutq@gmail.com>
2025-03-29 20:28:56 -0700

    Updated experimental GUI

--- Launcher/LauncherGUI.java
+++ Launcher/LauncherGUI.java
@@ -8,7 +8,7 @@ import java.awt.*;
 public class LauncherGUI {
     public static void launch(){
         JFrame frame = new JFrame("HappyHex Version " + LaunchEssentials.currentGameVersion);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.setBackground(Color.WHITE);
         frame.setSize(new Dimension(800, 800));
@@ -16,12 +16,35 @@ public class LauncherGUI {
 
         JPanel launchPanel = new JPanel();
         launchPanel.setLayout(new BoxLayout(launchPanel, BoxLayout.Y_AXIS));
+        launchPanel.add(Box.createVerticalGlue());
 
-        JLabel gameNameLabel = new JLabel("HappyHex");
-        gameNameLabel.setFont(new Font(GameEssentials.gameTitleFont, Font.BOLD, 20));
-        gameNameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        gameNameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        gameNameLabel.setForeground(Color.WHITE);
+        JPanel gameNameLabelPanel = new JPanel();
+        gameNameLabelPanel.setLayout(new BoxLayout(gameNameLabelPanel, BoxLayout.X_AXIS));
+        gameNameLabelPanel.add(Box.createHorizontalGlue());
+        JLabel[] gameNameLabels = new JLabel[12];
+        String gameNameString = "⬢HAPPY⬢⬢HEX⬢";
+        for (int i = 0; i < 12; i ++){
+            gameNameLabels[i] = new JLabel();
+            gameNameLabels[i].setFont(new Font(LaunchEssentials.launchTitleFont, Font.BOLD, 80));
+            gameNameLabels[i].setAlignmentY(Component.CENTER_ALIGNMENT);
+            gameNameLabels[i].setHorizontalAlignment(SwingConstants.CENTER);
+            gameNameLabels[i].setVerticalAlignment(SwingConstants.CENTER);
+            gameNameLabels[i].setText(gameNameString.substring(i, i+1));
+            gameNameLabels[i].setForeground(GameEssentials.getIndexedPieceColor((i*5)%12));
+            gameNameLabelPanel.add(gameNameLabels[i]);
+        }
+        gameNameLabelPanel.add(Box.createHorizontalGlue());
+        launchPanel.add(gameNameLabelPanel);
+
+        JLabel launchVersionLabel = new JLabel("Version " + LaunchEssentials.currentGameVersion);
+        launchVersionLabel.setFont(new Font(LaunchEssentials.launchVersionFont, Font.PLAIN, 40));
+        launchVersionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        launchVersionLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        launchPanel.add(launchVersionLabel);
+
+        launchPanel.add(Box.createVerticalGlue());
+        launchPanel.add(new LaunchButton());
+        launchPanel.add(Box.createVerticalGlue());
 
         frame.add(launchPanel);
         frame.setVisible(true);
