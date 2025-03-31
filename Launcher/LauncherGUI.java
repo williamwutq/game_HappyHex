461299b22b8a16f56311f59fccaa1bc28892b9d5
williamwutq <williamwutq@gmail.com>
2025-03-30 17:41:06 -0700

    Split initialization code into callable sections in LauncherGUI.java

--- Launcher/LauncherGUI.java
+++ Launcher/LauncherGUI.java
@@ -5,23 +5,48 @@ import GUI.GameEssentials;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class LauncherGUI {
+    public static JFrame mainFrame;
     public static void launch(){
-        JFrame frame = new JFrame("HappyHex Version " + LaunchEssentials.currentGameVersion);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setBackground(LaunchEssentials.launchBackgroundColor);
-        frame.setSize(new Dimension(400, 400));
-        frame.setMinimumSize(new Dimension(400, 400));
-
-        JPanel launchPanel = new LaunchPanel();
-
-        frame.add(launchPanel);
-        frame.validate();
-        frame.setVisible(true);
-        frame.setSize(new Dimension(800, 800));
-        frame.repaint();
+        setupMainFrame();
+        mainFrame.add(fetchLaunchPanel());
+        mainFrame.validate();
+        mainFrame.setVisible(true);
+        mainFrame.setSize(new Dimension(800, 800));
+        mainFrame.repaint();
+    }
+    public static JPanel fetchLaunchPanel(){
+        return new LaunchPanel();
+    }
+    private static void setupMainFrame(){
+        mainFrame = new JFrame("HappyHex Version " + Launcher.LaunchEssentials.currentGameVersion);
+        mainFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        mainFrame.setLayout(new BorderLayout());
+        mainFrame.setBackground(LaunchEssentials.launchBackgroundColor);
+        mainFrame.setSize(new Dimension(400, 400));
+        mainFrame.setMinimumSize(new Dimension(400, 400));
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                // Custom code to execute when the window is closing
+                // Log if it has score and reset
+                if(GameEssentials.turn != 0) {
+                    GameEssentials.logGame();
+                }
+                GameEssentials.resetGame();
+                // Close
+                mainFrame.dispose();
+            }
+        });
+    }
+    public static void removeAllFromFrame(){
+        mainFrame.removeAll();
+        mainFrame.setLayout(new BorderLayout());
+    }
+    public static void setBackgroundColor(Color color){
+        mainFrame.setBackground(color);
     }
     public static void main(String[] args){
         launch();
