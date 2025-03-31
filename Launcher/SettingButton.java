87bd626ba390216b94ae7fcc105f8dceab7228d6
williamwutq <williamwutq@gmail.com>
2025-03-31 13:35:45 -0700

    Created SettingButton.java

---
+++ Launcher/SettingButton.java
@@ -0,0 +1,12 @@
+package Launcher;
+
+import java.awt.event.ActionEvent;
+
+public class SettingButton extends LaunchButton {
+    public SettingButton() {
+        super("LOG IN");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+}
