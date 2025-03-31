9bdb717d10fb1635e024607e214144bada988f8b
williamwutq <williamwutq@gmail.com>
2025-03-31 13:35:37 -0700

    Created LoginButton.java

---
+++ Launcher/LoginButton.java
@@ -0,0 +1,12 @@
+package Launcher;
+
+import java.awt.event.*;
+
+public class LoginButton extends LaunchButton{
+    public LoginButton() {
+        super("LOG IN");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+    }
+}
