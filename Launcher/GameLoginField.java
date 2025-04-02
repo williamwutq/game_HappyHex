1361dd2c75824b4e1b22b0e1292dcb4434ae2b71
williamwutq <williamwutq@gmail.com>
2025-04-01 23:56:25 -0700

    Added prototype text field

--- Launcher/GameLoginField.java
+++ Launcher/GameLoginField.java
@@ -2,12 +2,31 @@ package Launcher;
 
 import Launcher.IO.Username;
 
+import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GameLoginField extends TextField implements ActionListener {
+public class GameLoginField extends JTextField implements ActionListener {
     public GameLoginField(){
         super(Username.MAX_LENGTH);
+        this.setBorder(new CompoundBorder(new LineBorder(Color.black, 2), new EmptyBorder(12,6,12,6)));
+        Dimension dimension = new Dimension(100,100);
+        this.setMaximumSize(dimension);
+        this.setMinimumSize(dimension);
+        this.setPreferredSize(dimension);
+        this.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setAlignmentY(Component.CENTER_ALIGNMENT);
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.addActionListener(this);
+    }
+
+    public void setDimension(int size){
+        this.setBorder(new CompoundBorder(new LineBorder(Color.black, 2), new EmptyBorder(size * 2, size,size *2 , size)));
+        Dimension dimension = new Dimension(size * Username.MAX_LENGTH + size * 2, size * 6);
+        this.setMaximumSize(dimension);
+        this.setMinimumSize(dimension);
+        this.setPreferredSize(dimension);
     }
 
     public void actionPerformed(ActionEvent e) {
--- Launcher/LoginPanel.java
+++ Launcher/LoginPanel.java
@@ -7,19 +7,12 @@ import java.awt.*;
 public class LoginPanel extends UniversalPanel {
     private JLabel launchEnterTextPrompt;
     private JLabel[] launchEnterTextRequirements;
-    private final String[] launchEnterText = {
-            "Rules:",
-            "1. The username must be between 3 - 24 characters long, inclusive.",
-            "2. The username can only contain 1-9, A-Z, a-z, dash, underline or spaces.",
-            "3. The username must contain at least one letter.",
-            "4. Special Symbols such as #%$ are not allowed.",
-            "5. The username cannot start or end with dash, underline or spaces.",
-            "6. The username cannot be one of the keywords used by the game system."
-    };
+    private GameLoginField textField;
     public LoginPanel(){
         super();
     }
     protected JComponent[] fetchContent() {
+        // Prompts
         launchEnterTextPrompt = new JLabel("Enter Username");
         launchEnterTextPrompt.setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.BOLD, 40));
         launchEnterTextPrompt.setForeground(LaunchEssentials.launchVersionFontColor);
@@ -27,7 +20,6 @@ public class LoginPanel extends UniversalPanel {
         launchEnterTextPrompt.setVerticalAlignment(SwingConstants.CENTER);
         launchEnterTextPrompt.setAlignmentX(Component.CENTER_ALIGNMENT);
         launchEnterTextPrompt.setBorder(new EmptyBorder(12, 0, 6, 0));
-
         String[] launchEnterText = {
                 "Rules:                                                       ",
                 "1. Must be between 3 - 24 characters long, inclusive.    ",
@@ -49,25 +41,30 @@ public class LoginPanel extends UniversalPanel {
             label.setAlignmentX(Component.CENTER_ALIGNMENT);
         }
         launchEnterTextRequirements[0].setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.BOLD, 20));
-        JTextField textField = new JTextField(Launcher.IO.Username.MAX_LENGTH);
+
+        // Text field
+        textField = new GameLoginField();
+        textField.setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.BOLD, 20));
         return new JComponent[]{(JComponent) Box.createVerticalGlue(), launchEnterTextPrompt, launchEnterTextRequirements[0],
                 launchEnterTextRequirements[1], launchEnterTextRequirements[2], launchEnterTextRequirements[3],
                 launchEnterTextRequirements[4], launchEnterTextRequirements[5], launchEnterTextRequirements[6],
-                textField, (JComponent) Box.createVerticalGlue()};
+                (JComponent) Box.createVerticalGlue(), textField, (JComponent) Box.createVerticalGlue()};
     }
     protected JComponent[] fetchHeader() {
         return null;
     }
     public void recalculate(){
         super.recalculate();
-        double referenceGameNameSize = Math.min(getReferenceHeight()*2, getReferenceWidth());
-        launchEnterTextPrompt.setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.ITALIC, (int)Math.round(referenceGameNameSize/24.0)));
-        launchEnterTextPrompt.setBorder(new EmptyBorder((int)Math.round(referenceGameNameSize/150.0)*2, 0, (int)Math.round(referenceGameNameSize/150.0), 0));
-
         double referenceEnterTextSize = Math.min(getReferenceHeight()*2, getReferenceWidth());
+        launchEnterTextPrompt.setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.ITALIC, (int)Math.round(referenceEnterTextSize/24.0)));
+        launchEnterTextPrompt.setBorder(new EmptyBorder((int)Math.round(referenceEnterTextSize/150.0)*2, 0, (int)Math.round(referenceEnterTextSize/150.0), 0));
+
         launchEnterTextRequirements[0].setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.BOLD, (int)Math.round(referenceEnterTextSize/48.0)));
         for (int i = 1; i < launchEnterTextRequirements.length; i++) {
             launchEnterTextRequirements[i].setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.PLAIN, (int)Math.round(referenceEnterTextSize/48.0)));
         }
+
+        textField.setFont(new Font(LaunchEssentials.launchEnterUsernameFont, Font.BOLD, (int)Math.round(referenceEnterTextSize/48.0)));
+        textField.setDimension((int)Math.round(referenceEnterTextSize/48.0));
     }
 }
