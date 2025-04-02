54b2a51cb78542697eca6973edb04cb8c58073e0
williamwutq <williamwutq@gmail.com>
2025-04-02 11:45:46 -0700

    Improved SlidingButtonPanel.java

--- Launcher/interactive/SlidingButtonPanel.java
+++ Launcher/interactive/SlidingButtonPanel.java
@@ -1,23 +1,29 @@
 package Launcher.interactive;
 
+import Launcher.LaunchEssentials;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
+
 public class SlidingButtonPanel extends JPanel {
     private boolean state;
     private String onText;
     private String offText;
-    public SlidingButtonPanel(){
+    private Color backgroundColor;
+    public SlidingButtonPanel(Color backgroundColor){
         super();
         this.state = false;
         this.onText = "ON";
         this.offText = "OFF";
+        this.backgroundColor = backgroundColor;
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
         this.setBorder(new LineBorder(Color.BLACK, 2));
         this.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.setAlignmentY(Component.CENTER_ALIGNMENT);
+        this.setBackground(backgroundColor);
         this.add(new SlidingButton());
         this.add(Box.createHorizontalGlue());
     }
@@ -25,6 +31,7 @@ public class SlidingButtonPanel extends JPanel {
         this.onText = onText;
         this.offText = offText;
     }
+
     protected void turnedOn(){
         this.remove(1);
         this.add(Box.createHorizontalGlue(), 0);
@@ -38,6 +45,11 @@ public class SlidingButtonPanel extends JPanel {
     private class SlidingButton extends JButton implements ActionListener{
         private SlidingButton(){
             this.setText(offText);
+            this.setForeground(backgroundColor);
+            this.setBackground(Color.WHITE);
+            this.setOpaque(true);
+            this.setFont(new Font(LaunchEssentials.launchSettingsSlidingButtonFont, Font.BOLD, 20));
+            this.setBorder(new EmptyBorder(0,0,0,0));
             this.setAlignmentX(Component.CENTER_ALIGNMENT);
             this.setAlignmentY(Component.CENTER_ALIGNMENT);
             this.addActionListener(this);
@@ -61,7 +73,7 @@ public class SlidingButtonPanel extends JPanel {
         frame.setSize(400,400);
         frame.setTitle("SlidingButtonPanel Tests");
         frame.setLayout(new BorderLayout());
-        frame.add(new SlidingButtonPanel(), BorderLayout.CENTER);
+        frame.add(new SlidingButtonPanel(Color.BLUE), BorderLayout.CENTER);
         frame.setVisible(true);
     }
 }
