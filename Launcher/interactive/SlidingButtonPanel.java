145d3d9d1819bbbe1ea302b381f19f7c13d6157b
williamwutq <williamwutq@gmail.com>
2025-04-02 12:35:19 -0700

    Improved SlidingButtonPanel.java

--- Launcher/interactive/SlidingButtonPanel.java
+++ Launcher/interactive/SlidingButtonPanel.java
@@ -8,44 +8,90 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class SlidingButtonPanel extends JPanel {
+public class SlidingButtonPanel extends JPanel implements ComponentListener {
     private boolean state;
     private String onText;
     private String offText;
-    private Color backgroundColor;
-    public SlidingButtonPanel(Color backgroundColor){
+    private SlidingButton button;
+    private Color onColor;
+    private Color offColor;
+    public SlidingButtonPanel(){
         super();
         this.state = false;
         this.onText = "ON";
         this.offText = "OFF";
-        this.backgroundColor = backgroundColor;
+        this.onColor = Color.GREEN;
+        this.offColor = Color.RED;
+        this.button = new SlidingButton();
+        this.setBackground(offColor);
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        this.setBorder(new LineBorder(Color.BLACK, 2));
+        this.setBorder(new EmptyBorder(0,0,0,0));
         this.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.setAlignmentY(Component.CENTER_ALIGNMENT);
-        this.setBackground(backgroundColor);
-        this.add(new SlidingButton());
+        this.add(button);
         this.add(Box.createHorizontalGlue());
+        this.addComponentListener(this);
     }
     public void setTexts(String onText, String offText){
         this.onText = onText;
         this.offText = offText;
     }
+    public void setBackground(Color on, Color off){
+        this.onColor = on;
+        this.offColor = off;
+    }
+    public void setBackground(Color color){
+        super.setBackground(color);
+        if(button != null) {
+            button.setForeground(color);
+        }
+    }
 
     protected void turnedOn(){
+        this.setBackground(onColor);
         this.remove(1);
         this.add(Box.createHorizontalGlue(), 0);
         this.revalidate();
     }
     protected void turnedOff(){
+        this.setBackground(offColor);
         this.remove(0);
         this.add(Box.createHorizontalGlue(), 1);
         this.revalidate();
     }
+    public void mandateSize(Dimension size) {
+        this.setPreferredSize(size);
+        this.setMinimumSize(size);
+        this.setMaximumSize(size);
+        recalculate();
+        repaint();
+    }
+    private void recalculate(){
+        Dimension dimension = new Dimension(this.getWidth()*2/3, this.getHeight());
+        button.setPreferredSize(dimension);
+        button.setMinimumSize(dimension);
+        button.setMaximumSize(dimension);
+        int size = Math.min(this.getWidth()*2/3, this.getHeight())/3;
+        button.setFont(new Font(LaunchEssentials.launchSettingsSlidingButtonFont, Font.BOLD, size));
+    }
+
+    public void componentResized(ComponentEvent e) {
+        recalculate();
+        repaint();
+    }
+    public void componentMoved(ComponentEvent e) {
+        recalculate();
+        repaint();
+    }
+    public void componentShown(ComponentEvent e) {
+        recalculate();
+        repaint();
+    }
+    public void componentHidden(ComponentEvent e) {}
+
     private class SlidingButton extends JButton implements ActionListener{
         private SlidingButton(){
             this.setText(offText);
-            this.setForeground(backgroundColor);
             this.setBackground(Color.WHITE);
             this.setOpaque(true);
             this.setFont(new Font(LaunchEssentials.launchSettingsSlidingButtonFont, Font.BOLD, 20));
@@ -59,10 +105,12 @@ public class SlidingButtonPanel extends JPanel {
             if(state){
                 state = false;
                 this.setText(offText);
+                this.setForeground(offColor);
                 turnedOff();
             } else {
                 state = true;
                 this.setText(onText);
+                this.setForeground(onColor);
                 turnedOn();
             }
         }
@@ -73,7 +121,10 @@ public class SlidingButtonPanel extends JPanel {
         frame.setSize(400,400);
         frame.setTitle("SlidingButtonPanel Tests");
         frame.setLayout(new BorderLayout());
-        frame.add(new SlidingButtonPanel(Color.BLUE), BorderLayout.CENTER);
+
+        JPanel buttonPanel = new SlidingButtonPanel();
+
+        frame.add(buttonPanel, BorderLayout.CENTER);
         frame.setVisible(true);
     }
 }
