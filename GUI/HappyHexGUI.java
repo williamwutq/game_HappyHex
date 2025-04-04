22b7a827be29170f582fdb10f0e9e4ad7d53eaa1
williamwutq <williamwutq@gmail.com>
2025-04-03 22:50:16 -0700

    Deleted troublesome labels intended to append to GamePanel in HappyHexGUI.java

--- GUI/HappyHexGUI.java
+++ GUI/HappyHexGUI.java
@@ -22,46 +22,7 @@ public class HappyHexGUI{
         GameEssentials.calculateButtonSize();
     }
     public static JPanel fetchGamePanel(){
-        GamePanel gamePanel = new GamePanel();
-        JLabel scoreLabel = new JLabel("SCORE 0", SwingConstants.CENTER){
-            @Override
-            public void paint(java.awt.Graphics g){
-                this.setText("SCORE " + GameEssentials.score);
-                this.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, (int)(HexButton.getActiveSize()/2)));
-                this.setBounds(gamePanel.getWidth() - (int)((GameEssentials.engine().getRadius()-0.5) * HexButton.getActiveSize()), 5, (int)((GameEssentials.engine().getRadius()-0.5) * HexButton.getActiveSize()), (int)(HexButton.getActiveSize()));
-                super.paintComponent(g);
-            }
-        };
-        JLabel turnLabel = new JLabel("TURN 0", SwingConstants.CENTER){
-            @Override
-            public void paint(java.awt.Graphics g){
-                this.setText("TURN " + GameEssentials.turn);
-                this.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, (int)(HexButton.getActiveSize()/2)));
-                this.setBounds(0, 5, (int)((GameEssentials.engine().getRadius()-0.5) * HexButton.getActiveSize()), (int)(HexButton.getActiveSize()));
-                super.paintComponent(g);
-            }
-        };
-        JLabel playerLabel = new JLabel(LaunchEssentials.getCurrentPlayer(), SwingConstants.CENTER){
-            @Override
-            public void paint(java.awt.Graphics g){
-                this.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, (int)(HexButton.getActiveSize()/2)));
-                this.setBounds(gamePanel.getWidth() - (int)((GameEssentials.engine().getRadius()-0.5) * HexButton.getActiveSize()), gamePanel.getHeight() - (int)Math.round(1.5 * HexButton.getActiveSize()), (int)((GameEssentials.engine().getRadius()-0.5) * HexButton.getActiveSize()), (int)(HexButton.getActiveSize()));
-                super.paintComponent(g);
-            }
-        };
-        scoreLabel.setForeground(GameEssentials.gameDisplayFontColor);
-        scoreLabel.setBounds(0, 0, 1, 1);
-        scoreLabel.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, 1));
-        turnLabel.setForeground(GameEssentials.gameDisplayFontColor);
-        turnLabel.setBounds(0, 0, 1, 1);
-        turnLabel.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, 1));
-        playerLabel.setForeground(GameEssentials.gameDisplayFontColor);
-        playerLabel.setBounds(0, 0, 1, 1);
-        playerLabel.setFont(new Font(GameEssentials.gameDisplayFont, Font.BOLD, 1));
-        gamePanel.add(turnLabel);
-        gamePanel.add(scoreLabel);
-        gamePanel.add(playerLabel);
-        return gamePanel;
+        return new GamePanel();
     }
     public static JPanel fetchPiecePanel(){
         return new PiecePanel();
