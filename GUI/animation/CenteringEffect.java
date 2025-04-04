1e0cd0a87a8b3e750967b1fa1f0e69976402c8fd
williamwutq <williamwutq@gmail.com>
2025-04-04 14:57:11 -0700

    Implemented CenteringEffect.java

--- GUI/animation/CenteringEffect.java
+++ GUI/animation/CenteringEffect.java
@@ -1,25 +1,38 @@
 package GUI.animation;
 
 
+import GUI.GameEssentials;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class CenteringEffect extends Animation {
-    private int x;
-    public CenteringEffect(int totalFrames, int frameTime, int x){
+    public CenteringEffect(int totalFrames, int frameTime){
         super(totalFrames, frameTime);
-        this.x = x;
-    }
-    public CenteringEffect(int totalFrames, int frameTime, int x, ActionListener listener){
-        super(totalFrames, frameTime, listener);
-        this.x = x;
     }
     @Override
     protected void paintFrame(java.awt.Graphics g, double progress){
-        g.setColor(Color.BLACK);
-        g.fillRect(x, (int)(progress * 150), 50, 50);
+        int x = 0;
+        int y = 0;
+        int size = 40;
+        double fill = 1.1 - (progress*0.2);
+        int[] xPoints = new int[14];
+        int[] yPoints = new int[14];
+        for (int i = 0; i < 7; i++) {
+            double angle = Math.toRadians(60 * i);
+            xPoints[i] = (int) Math.round(size * (x * 2 + GameEssentials.sinOf60 + Math.sin(angle) * 0.9));
+            yPoints[i] = (int) Math.round(size * (y * 2 + 1.0 + Math.cos(angle) * 0.9));
+        }
+        for (int i = 0; i < 7; i++) {
+            double angle = Math.toRadians(60 * i);
+            xPoints[i + 7] = (int) Math.round(size * (x * 2 + GameEssentials.sinOf60 + Math.sin(angle) * fill));
+            yPoints[i + 7] = (int) Math.round(size * (y * 2 + 1.0 + Math.cos(angle) * fill));
+        }
+        g.setColor(Color.RED);
+        g.fillPolygon(xPoints, yPoints, 14);
     }
     public static void main(String[] args){
         JFrame mainFrame = new JFrame("CenteringEffectTest");
@@ -29,18 +42,12 @@ public class CenteringEffect extends Animation {
         mainFrame.setSize(400, 400);
         mainFrame.setBackground(Color.CYAN);
         mainPanel.setBackground(Color.CYAN);
-        Animation effect = new CenteringEffect(1000, 1, 0);
-        Animation effect2 = new CenteringEffect(1000, 1, 50);
+        Animation effect = new CenteringEffect(300, 1);
         effect.setPreferredSize(new Dimension(400, 400));
-        effect2.setPreferredSize(new Dimension(400, 400));
         effect.setSize(new Dimension(400, 400));
-        effect2.setSize(new Dimension(400, 400));
         effect.setBounds(new Rectangle(0, 0, 400, 400));
-        effect2.setBounds(new Rectangle(0, 0, 400, 400));
-        effect.setEndListener(effect2.getStartListener());
 
         mainPanel.add(effect);
-        mainPanel.add(effect2);
         mainFrame.add(mainPanel);
         mainFrame.setVisible(true);
         effect.start();
