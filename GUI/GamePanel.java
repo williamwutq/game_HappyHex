b21c37c5ad7020d1ba0854c68734d26b7017ed94
williamwutq <williamwutq@gmail.com>
2025-04-03 23:42:18 -0700

    Added basic information labels in GamePanel.java

--- GUI/GamePanel.java
+++ GUI/GamePanel.java
@@ -11,6 +11,19 @@ public class GamePanel extends JPanel {
         for (int i = 0; i < GameEssentials.engine().length(); i++) {
             this.add(new EngineButton(i));
         }
+        // Construct labels
+        GameInfoPanel turnLabel = new GameInfoPanel();
+        GameInfoPanel scoreLabel = new GameInfoPanel();
+        GameInfoPanel playerLabel = new GameInfoPanel();
+        turnLabel.setTitle("TURN");
+        scoreLabel.setTitle("SCORE");
+        playerLabel.setTitle("PLAYER");
+        turnLabel.setBounds(0, 0, 100, 100);
+        scoreLabel.setBounds(300, 0, 100, 100);
+        playerLabel.setBounds(300, 300, 100, 100);
+        this.add(turnLabel);
+        this.add(scoreLabel);
+        this.add(playerLabel);
     }
     public void paint(java.awt.Graphics g) {
         GameEssentials.calculateButtonSize();
