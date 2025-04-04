89ccde21bcf47a5b423f68b4572c88068d8d4b76
williamwutq <williamwutq@gmail.com>
2025-04-03 22:46:54 -0700

    Bump version up to 0.4.2

--- Launcher/LaunchEssentials.java
+++ Launcher/LaunchEssentials.java
@@ -13,7 +13,7 @@ import java.io.IOException;
  */
 public final class LaunchEssentials {
     // Program info
-    public static final GameVersion currentGameVersion = new GameVersion(0, 4, 1);
+    public static final GameVersion currentGameVersion = new GameVersion(0, 4, 2);
     public static final String currentGameName = "HappyHex";
     public static final String currentEnvironment = "java";
 
