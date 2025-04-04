586e8744e9a68287ac36d0ea526a75efe5b4347b
williamwutq <williamwutq@gmail.com>
2025-04-04 16:19:20 -0700

    Modified painting for better looks

--- GUI/animation/CenteringEffect.java
+++ GUI/animation/CenteringEffect.java
@@ -24,36 +24,36 @@ public class CenteringEffect extends Animation {
             double size = GUI.HexButton.getActiveSize();
             int width = (int) Math.round(2 * size * GameEssentials.sinOf60);
             int height = (int) Math.round(2 * size);
+            double extended = 1.2;
             Dimension dimension = new Dimension(width, height);
             this.setSize(dimension);
             this.setMinimumSize(dimension);
             this.setMaximumSize(dimension);
             this.setPreferredSize(dimension);
-            int x = (int) Math.round(size * 2 * block.X());
-            int y = (int) Math.round(size * 2 * (block.Y() + (GameEssentials.engine().getRadius() - 1) * 0.75));
-            this.setBounds(x + GameEssentials.getGamePanelWidthExtension(), y + GameEssentials.getGamePanelHeightExtension(), width, height);
+            int x = (int) Math.round(size * 2 * block.X() + (1 - extended) * size);
+            int y = (int) Math.round(size * 2 * (block.Y() + (GameEssentials.engine().getRadius() - 1) * 0.75) + (1 - extended) * size);
+            this.setBounds(x + GameEssentials.getGamePanelWidthExtension(), y + GameEssentials.getGamePanelHeightExtension(), (int) Math.round(extended * 2 * size * GameEssentials.sinOf60), (int) Math.round(extended * 2 * size));
         }
     }
     @Override
     protected void paintFrame(java.awt.Graphics g, double progress){
         resetSize();
-        int x = 0;
-        int y = 0;
         double size = GUI.HexButton.getActiveSize();
-        double fill = 1.1 - (progress*0.2);
+        double extended = 1.2;
+        double fill = extended - (progress*0.3);
         int[] xPoints = new int[14];
         int[] yPoints = new int[14];
         for (int i = 0; i < 7; i++) {
             double angle = Math.toRadians(60 * i);
-            xPoints[i] = (int) Math.round(size * (x * 2 + GameEssentials.sinOf60 + Math.sin(angle) * 0.9));
-            yPoints[i] = (int) Math.round(size * (y * 2 + 1.0 + Math.cos(angle) * 0.9));
+            xPoints[i] = (int) Math.round(size * ((extended - 1) + GameEssentials.sinOf60 + Math.sin(angle) * 0.9));
+            yPoints[i] = (int) Math.round(size * ((extended - 1) + 1.0 + Math.cos(angle) * 0.9));
         }
         for (int i = 0; i < 7; i++) {
             double angle = Math.toRadians(60 * i);
-            xPoints[i + 7] = (int) Math.round(size * (x * 2 + GameEssentials.sinOf60 + Math.sin(angle) * fill));
-            yPoints[i + 7] = (int) Math.round(size * (y * 2 + 1.0 + Math.cos(angle) * fill));
+            xPoints[i + 7] = (int) Math.round(size * ((extended - 1) + GameEssentials.sinOf60 + Math.sin(angle) * fill));
+            yPoints[i + 7] = (int) Math.round(size * ((extended - 1) + 1.0 + Math.cos(angle) * fill));
         }
-        g.setColor(GameEssentials.darkenColor(block.color()));
+        g.setColor(GameEssentials.whitenColor(GameEssentials.dimColor(block.color())));
         g.fillPolygon(xPoints, yPoints, 14);
     }
 }
