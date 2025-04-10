9fc4cd5b179c655ec6c38edc24469d7e786ebffa
williamwutq <williamwutq@gmail.com>
2025-04-09 17:20:51 -0700

    Changed display text to "New" instead of "New Game"

--- Launcher/interactive/NextGameButton.java
+++ Launcher/interactive/NextGameButton.java
@@ -2,7 +2,7 @@ package Launcher.interactive;
 
 public class NextGameButton extends GUI.SimpleButton {
     public NextGameButton(){
-        super("NEW GAME", Launcher.LaunchEssentials.launchNewButtonBackgroundColor);
+        super("NEW", Launcher.LaunchEssentials.launchNewButtonBackgroundColor);
     }
     protected void clicked() {
         Launcher.LauncherGUI.startGame();
