68d9e17abfb4c261a4ab686d9f4bceaca8d71592
williamwutq <williamwutq@gmail.com>
2025-04-15 02:18:03 -0700

    Added javadoc to JsonConvertible.java

--- Launcher/IO/JsonConvertible.java
+++ Launcher/IO/JsonConvertible.java
@@ -3,11 +3,27 @@ package Launcher.IO;
 import javax.json.*;
 
 /**
- * An object that can have a JSON representation
+ * <p> An interface for converting objects into JSON representations.</p>
+ * <p> Implementing classes must provide a {@link #toJsonObjectBuilder() method} to convert
+ * an object of that class into a {@link javax.json.JsonObjectBuilder}. The interface also
+ * provides method to convert into a {@link javax.json.JsonObject}.</p>
  * @see JsonObject
  */
 public interface JsonConvertible {
+    /**
+     * Converts the object into a {@link javax.json.JsonObjectBuilder}.
+     * This method should be implemented by classes to define how their data
+     * should be serialized into a JSON structure.
+     *
+     * @return a JsonObjectBuilder containing the object's data
+     */
     abstract JsonObjectBuilder toJsonObjectBuilder();
+    /**
+     * Converts the object into a {@link javax.json.JsonObject} by building the result
+     * from the {@link #toJsonObjectBuilder()}.
+     *
+     * @return a JsonObject representing the object
+     */
     default JsonObject toJsonObject(){
         return toJsonObjectBuilder().build();
     }
