5f5aa1f807e843542e268e53345082132ef3d67a
williamwutq <williamwutq@gmail.com>
2025-04-16 15:40:45 -0700

    Modified special feature generation code to accept hints; added GodMode to FeatureFactory

--- special/FeatureFactory.java
+++ special/FeatureFactory.java
@@ -1,17 +1,24 @@
 package special;
 
 public class FeatureFactory {
-    public static SpecialFeature createFeature(String className){
+    public static SpecialFeature createFeature(String className, String hint){
         if(className.equals("Color") || className.equals("java.awt.Color")) {
             SpecialFeature grayscale = new special.Styles.Grayscale();
             java.time.LocalDate date = java.time.LocalDate.now();
-            if ((date.getMonthValue() == 9 && date.getDayOfMonth() == 11) || Math.random() >= 0.94) {
+            if ((date.getMonthValue() == 9 && date.getDayOfMonth() == 11) || Math.random() >= 0.94 || hint.equals("9/11")) {
                 return grayscale;
             }
         } else if (className.equals("Piece") || className.equals("Hex.Piece")){
-            return new special.Logic.HardMode();
+            if(hint.equals("God")){
+                return new special.Logic.GodMode();
+            } else if (hint.equals("Hard")){
+                return new special.Logic.HardMode();
+            }
         }
         return new DefaultFeature(); // Default feature do nothing
+    }
+    public static SpecialFeature createFeature(String className){
+        return createFeature(className, "Default");
     }
     public static SpecialFeature createFeature(){
         return new DefaultFeature(); // Default feature do nothing
