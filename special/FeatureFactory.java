4fd2e13856a9040d69d9390e868c750fda905136
williamwutq <williamwutq@gmail.com>
2025-04-17 01:19:03 -0700

    Added FilledWithLove.java to activate Feb 14

--- special/FeatureFactory.java
+++ special/FeatureFactory.java
@@ -3,13 +3,17 @@ package special;
 public class FeatureFactory {
     public static SpecialFeature createFeature(String className){
         if(className.equals("Color") || className.equals("java.awt.Color")) {
-            SpecialFeature grayscale = new special.Styles.Grayscale();
             java.time.LocalDate date = java.time.LocalDate.now();
             if ((date.getMonthValue() == 9 && date.getDayOfMonth() == 11) || Math.random() >= 0.94) {
-                return grayscale;
+                return new special.Styles.Grayscale();
             }
         } else if (className.equals("Piece") || className.equals("Hex.Piece")){
             return new special.Logic.HardMode();
+        } else if (className.equals("Animation") || className.equals("GUI.animation.Animation")){
+            java.time.LocalDate date = java.time.LocalDate.now();
+            if ((date.getMonthValue() == 2 && date.getDayOfMonth() == 14)) {
+                return new special.Valentine.FilledWithLove();
+            }
         }
         return new DefaultFeature(); // Default feature do nothing
     }
