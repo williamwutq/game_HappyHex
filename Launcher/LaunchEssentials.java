35c170e6f8e892bb02a2c93fe757044978e5bb85
williamwutq <williamwutq@gmail.com>
2025-04-17 18:12:30 -0700

    Update Version

--- Launcher/LaunchEssentials.java
+++ Launcher/LaunchEssentials.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  */
 public final class LaunchEssentials {
     // Program info
-    public static final GameVersion currentGameVersion = new GameVersion(1, 1, 2);
+    public static final GameVersion currentGameVersion = new GameVersion(1, 1, 3);
     public static final String currentGameName = "HappyHex";
     public static final String currentEnvironment = "java";
 
