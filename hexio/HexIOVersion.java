991c2dfbca865c899264cb4df545f94ff6be1ffe
williamwutq <williamwutq@gmail.com>
2025-04-23 01:30:56 -0700

    Added javadoc to HexIOVersion

--- hexio/HexIOVersion.java
+++ hexio/HexIOVersion.java
@@ -1,7 +1,11 @@
 package hexio;
 
+/** This is a static final class storing the version information for this package. */
 public final class HexIOVersion {
+    /** The major version number of the current {@link hexio}. */
     public static final int major = 1;
+    /** The minor version number of the current {@link hexio}. */
     public static final int minor = 2;
+    /** The patch version number of the current {@link hexio}. */
     public static final int patch = 2;
 }
