3403dcd2046ea01eb34124f0d2010717545b4337
williamwutq <williamwutq@gmail.com>
2025-04-23 01:38:59 -0700

    Added game information and renamed to HexIOInfo

--- hexio/HexIOInfo.java
+++ hexio/HexIOInfo.java
@@ -1,11 +1,15 @@
 package hexio;
 
 /** This is a static final class storing the version information for this package. */
-public final class HexIOVersion {
+public final class HexIOInfo {
     /** The major version number of the current {@link hexio}. */
     public static final int major = 1;
     /** The minor version number of the current {@link hexio}. */
     public static final int minor = 2;
     /** The patch version number of the current {@link hexio}. */
     public static final int patch = 2;
+    /** The name of the current running game. */
+    public static final String gameName = "HappyHex";
+    /** The evironment of the current running game. */
+    public static final String gameEnvironment = "java";
 }
