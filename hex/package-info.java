e7b8e313acdb275f589ba82956339d65147fe80d
williamwutq <williamwutq@gmail.com>
2025-05-03 21:41:37 -0700

    Update javadoc for hex

--- hex/package-info.java
+++ hex/package-info.java
@@ -1,10 +1,11 @@
 /**
- * Provides classes and interfaces for managing a hexagonal grid system,
+ * Provides classes and interfaces for managing a hexagonal grid system and colors,
  * including coordinate calculations, game engine operations, and piece queuing.
  * <p>
  * This package includes:
  * <ul>
  *   <li>{@link hex.Hex} – Represents 2D coordinates in a hexagonal grid and provides methods for raw and line-based computations.</li>
+ *   <li>{@link hex.SolidColor} – Wrapper of {@code Color} to exclude explicit transparency and provide indexed coloring system.</li>
  *   <li>{@link hex.Block} – Extends {@code hex} to include a color and an occupancy state for blocks in the grid.</li>
  *   <li>{@link hex.HexGrid} – Defines the interface for grids composed of {@code Block} elements.</li>
  *   <li>{@link hex.Piece} – Models a game piece as a collection of {@code Block} objects arranged as a smaller hex grid.</li>
@@ -14,6 +15,6 @@
  * </p>
  * @since 1.0
  * @author William Wu
- * @version 1.2
+ * @version 1.3
  */
 package hex;
