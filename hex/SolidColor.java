a408483fa91303cfa4e4e40fc22ad78534400637
williamwutq <williamwutq@gmail.com>
2025-05-03 21:12:16 -0700

    Added more color methods

--- hex/SolidColor.java
+++ hex/SolidColor.java
@@ -24,19 +24,42 @@
 
 package hex;
 
+import java.util.Objects;
+
 public class SolidColor {
     int index;
     int red;
     int green;
     int blue;
     public SolidColor(java.awt.Color color){
-        this.index = -2; // Out of group
+        this.index = -3; // Out of group
         this.red = color.getRed();
         this.green = color.getGreen();
         this.blue = color.getBlue();
     }
     public SolidColor(int red, int green, int blue) {
-        this.index = -2; // Out of group
+        this.index = -3; // Out of group
+        if (red < 0) {
+            red = 0;
+        } else if (red > 255) {
+            red = 255;
+        }
+        if (green < 0) {
+            green = 0;
+        } else if (green > 255) {
+            green = 255;
+        }
+        if (blue < 0) {
+            blue = 0;
+        } else if (blue > 255) {
+            blue = 255;
+        }
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+    private SolidColor(int index, int red, int green, int blue) {
+        this.index = index;
         if (red < 0) {
             red = 0;
         } else if (red > 255) {
@@ -68,7 +91,24 @@ public class SolidColor {
     public java.awt.Color getColor(){
         return new java.awt.Color(red, green, blue);
     }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof SolidColor that)) return false;
+        return red == that.red && green == that.green && blue == that.blue;
+    }
+    public int hashCode() {
+        return Objects.hash(red, green, blue);
+    }
     public SolidColor clone(){
         return new SolidColor(red, green, blue);
     }
+    public static final SolidColor BLACK = new SolidColor(-2, 0, 0, 0);
+    public static final SolidColor RED = new SolidColor(-2, 255, 0, 0);
+    public static final SolidColor YELLOW = new SolidColor(-2, 255, 255, 0);
+    public static final SolidColor GREEN = new SolidColor(-2, 0, 255, 0);
+    public static final SolidColor CYAN = new SolidColor(-2, 0, 255, 255);
+    public static final SolidColor BLUE = new SolidColor(-2, 0, 0, 255);
+    public static final SolidColor MAGENTA = new SolidColor(-2, 255, 0, 255);
+    public static final SolidColor WHITE = new SolidColor(-2, 255, 255, 255);
 }
