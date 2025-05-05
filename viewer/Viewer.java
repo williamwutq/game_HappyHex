60ddf1df668b975c9f2abcbddd58cd04084e976b
williamwutq <williamwutq@gmail.com>
2025-05-04 19:11:46 -0700

    Created viewer main

---
+++ viewer/Viewer.java
@@ -0,0 +1,12 @@
+package viewer;
+
+/**
+ * Main file of Game Viewer, which is used as a tool to view past games by reading and showing game files.
+ * <p>
+ * Run {@link #main} to launch the game viewer.
+ */
+public class Viewer {
+    public static void main(String[] args){
+
+    }
+}
