5cb5bcf5c0b48bda5348faa01e7e8a2c7f77565d
williamwutq <williamwutq@gmail.com>
2025-05-07 00:46:18 -0700

    Add getters

--- viewer/logic/Tracker.java
+++ viewer/logic/Tracker.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 import java.io.IOException;
 
 public class Tracker {
-    HexEngine[] engines;
-    Hex[] origins;
-    Piece[][] queues;
-    int[] scores;
-    int length;
+    private final HexEngine[] engines;
+    private final Hex[] origins;
+    private final Piece[][] queues;
+    private final int[] scores;
+    private final int length;
     /**
      * Create a {@code Tracker} object to track the progression of a game by reading game
      * information from a {@link HexLogger}. This logger need to contain valid game information,
@@ -85,6 +85,128 @@ public class Tracker {
 
         sb.append("}]");
         return sb.toString();
+    }
+    /**
+     * Returns game {@link HexEngine board} array stored in the {@code Tracker}, containing completed game board data
+     * across all game states by chronological order, starting from the initial state, where the {@code board}
+     * will be {@link HexEngine#HexEngine(int) empty}.
+     * <p>
+     * The length of the array returned will be {@link #length}.
+     *
+     * @return an array of {@link HexEngine} representing the game board across all game states
+     */
+    public HexEngine[] engines() {
+        return engines.clone();
+    }
+    /**
+     * Returns {@link Hex coordinate} array stored in the {@code Tracker}, containing {@link Piece} placement
+     * {@code coordinates} in the {@link HexEngine} across all game states by chronological order, starting
+     * from the initial state, where the placement coordinate will be {@code null}.
+     * <p>
+     * The length of the array returned will be {@link #length}.
+     *
+     * @return an array of {@link Hex coordinates} representing the game piece placement across all game states
+     */
+    public Hex[] origins() {
+        return origins.clone();
+    }
+    /**
+     * Returns {@link Piece} queue array stored in the {@code Tracker}, containing {@code Piece} queues
+     * across all game states by chronological order, starting from the initial state.
+     * <p>
+     * The length of the array returned will be {@link #length}.
+     *
+     * @return an array of {@code Piece} arrays representing the game queue across all game states
+     */
+    public Piece[][] queues() {
+        return queues.clone();
+    }
+    /**
+     * Returns score array stored in the {@code Tracker}, containing scores across all game states
+     * by chronological order, starting from the initial state, where score is 0.
+     * <p>
+     * The length of the array returned will be {@link #length}.
+     *
+     * @return an array of {@code int} representing the game score across all game states
+     */
+    public int[] scores() {
+        return scores.clone();
+    }
+    /**
+     * Returns the total length of game states stored in the {@code Tracker}, including the initial game state.
+     * This value will be one greater than the total game moves.
+     *
+     * @return the total length of all game states, which is the length of all arrays in this {@code Tracker}
+     */
+    public int length() {
+        return length;
+    }
+    /**
+     * Returns the game {@link HexEngine board} at a specific indexed game state stored in the {@code Tracker}.
+     *
+     * @param index the index of the game state, with 0 representing the initial state and {@link #length}-1
+     *              representing the final game state. Index must be valid.
+     * @return a {@link HexEngine} representing the game board in the indexed game state
+     * @throws IndexOutOfBoundsException if the index is not in range [0, length)
+     * @see #engines()
+     */
+    public HexEngine engineAt(int index) throws IndexOutOfBoundsException{
+        checkIndex(index);
+        return engines[index];
+    }
+    /**
+     * Returns the game {@link Piece} placement {@link Hex coordinate} at
+     * a specific indexed game state stored in the {@code Tracker}.
+     *
+     * @param index the index of the game state, with 0 representing the initial state and {@link #length}-1
+     *              representing the final game state. Index must be valid
+     * @return a {@link Hex} coordinate representing the game piece placement in the indexed game state
+     * @throws IndexOutOfBoundsException if the index is not in range (0, length)
+     * @see #origins()
+     */
+    public Hex originAt(int index) {
+        if (index == 0) throw new ArrayIndexOutOfBoundsException("Index 0 out of bounds because move at game state 0 does not exist");
+        checkIndex(index);
+        return origins[index];
+    }
+    /**
+     * Returns the game {@link Piece} queue at a specific indexed game state stored in the {@code Tracker}.
+     *
+     * @param index the index of the game state, with 0 representing the initial state and {@link #length}-1
+     *              representing the final game state. Index must be valid
+     * @return an array of {@link Piece} representing the game {@code Piece} queue in the indexed game state
+     * @throws IndexOutOfBoundsException if the index is not in range [0, length)
+     * @see #queues()
+     */
+    public Piece[] queueAt(int index) {
+        checkIndex(index);
+        return queues[index];
+    }
+    /**
+     * Returns the game score at a specific indexed game state stored in the {@code Tracker}.
+     *
+     * @param index the index of the game state, with 0 representing the initial state and {@link #length}-1
+     *              representing the final game state. Index must be valid
+     * @return an integer representing the game score queue in the indexed game state
+     * @throws IndexOutOfBoundsException if the index is not in range [0, length)
+     * @see #scores()
+     */
+    public int scoreAt(int index) {
+        checkIndex(index);
+        return scores[index];
+    }
+    /**
+     * Check whether the index passed in for accessing elements in this game {@code Tracker} is valid.
+     * If the passed in index is not valid, it will throw an {@link IndexOutOfBoundsException}. This method is intended
+     * to catch out-of-bounds index early to avoid further problems.
+     *
+     * @param index the index to check
+     * @throws IndexOutOfBoundsException if the index is not in range [0, length)
+     */
+    private void checkIndex(int index) throws IndexOutOfBoundsException{
+        if (index < 0 || index >= length) {
+            throw new ArrayIndexOutOfBoundsException("Index " + index + " out of bounds for length " + length);
+        }
     }
     public static void main(String[] args) throws IOException {
         HexLogger logger = HexLogger.generateBinaryLoggers().getFirst();
