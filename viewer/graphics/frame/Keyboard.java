e7ea33e4a7f405af7624fc7b474951f5df97b2f7
williamwutq <williamwutq@gmail.com>
2025-05-07 16:57:00 -0700

    Improvements

--- viewer/graphics/frame/Keyboard.java
+++ viewer/graphics/frame/Keyboard.java
@@ -30,72 +30,59 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Keyboard extends JComponent {
-    private final static String font = "Courier";
+    private static final String keyFont = "Courier";
+    private static final Color keyColor = new Color(85, 85, 85);
     private double size;
     private ActionListener listener;
     private Key[][] keys;
     public Keyboard(ActionListener listener){
         this.listener = listener;
-        this.setBackground(new Color(85, 85, 85));
+        this.setBackground(keyColor);
         this.setLayout(null);
-        // Construct keys
+        this.setDoubleBuffered(true);
+        // Key initialization
+        String[][] labels = {
+                {" 0 ", " 1 ", " 2 ", " 3 ", " 4 "},
+                {" 5 ", " 6 ", " 7 ", " 8 ", " 9 "},
+                {" A ", " B ", " C ", " - ", " + "},
+                {" D ", " E ", " F ", "DEL", "CLR"},
+                {"HOM", " < ", " > ", "CNF", "ENT"}
+        };
         keys = new Key[5][5];
-        keys[0][0] = new Key(" 0 ");
-        keys[0][1] = new Key(" 1 ");
-        keys[0][2] = new Key(" 2 ");
-        keys[0][3] = new Key(" 3 ");
-        keys[0][4] = new Key(" 4 ");
-        keys[1][0] = new Key(" 5 ");
-        keys[1][1] = new Key(" 6 ");
-        keys[1][2] = new Key(" 7 ");
-        keys[1][3] = new Key(" 8 ");
-        keys[1][4] = new Key(" 9 ");
-        keys[2][0] = new Key(" A ");
-        keys[2][1] = new Key(" B ");
-        keys[2][2] = new Key(" C ");
-        keys[2][3] = new Key(" - ");
-        keys[2][4] = new Key(" + ");
-        keys[3][0] = new Key(" D ");
-        keys[3][1] = new Key(" E ");
-        keys[3][2] = new Key(" F ");
-        keys[3][3] = new Key("DEL");
-        keys[3][4] = new Key("CLR");
-        keys[4][0] = new Key("HOM");
-        keys[4][1] = new Key(" < ");
-        keys[4][2] = new Key(" > ");
-        keys[4][3] = new Key("CNF");
-        keys[4][4] = new Key("ENT");
-        for (int i = 0; i < keys.length; i++) {
-            for (int j = 0; j < keys[i].length; j++) {
-                keys[i][j].setBounds(j,i,1,1);
-                this.add(keys[i][j]);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Key key = new Key(labels[i][j]);
+                keys[i][j] = key;
+                add(key);
             }
         }
     }
+
     /**
-     * Reset the size of individual keys in the panel by to match the maximum size allowed in this panel.
+     * Recalculates key layout and size when the component is resized.
      */
-    public void resetSize(){
-        // Calculate size
-        int height = getHeight()-6;
-        int width = getWidth()-6;
-        size = (Math.min(height * 0.2, width * 0.2));
+    public void doLayout() {
+        int halfHeight = getHeight()/2;
+        int halfWidth = getWidth()/2;
+        size = Math.min((getHeight() - 6) * 0.19, (getWidth() - 6) * 0.19);
         int sizeInt = (int) size;
+        int size04 = (int)(size * 0.4);
+
         for (int i = 0; i < keys.length; i++) {
-            int sizedI = 3 + (int)((i-2.5)*size) + height/2;
+            int y = (int) ((i - 2.5) * size) + halfHeight;
             for (int j = 0; j < keys[i].length; j++) {
-                Key key = keys[i][j];
-                key.setBounds(3 + (int)((j-2.5)*size)+ width/2, sizedI, sizeInt, sizeInt);
-                key.resetSize();
+                int x = (int) ((j - 2.5) * size) + halfWidth;
+                keys[i][j].setBounds(x, y, sizeInt, sizeInt);
+                keys[i][j].setFont(new Font(keyFont, Font.BOLD, size04));
             }
         }
     }
     public void paint(Graphics g){
-        resetSize();
         Graphics2D g2 = (Graphics2D) g.create();
+        int sizeInt = (int) size;
         g2.setColor(this.getBackground());
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), (int) size, (int) size);
+        g2.fillRoundRect(3, 3, getWidth()-6, getHeight()-6, sizeInt, sizeInt);
         g2.dispose();
         paintChildren(g);
     }
@@ -103,30 +90,39 @@ public class Keyboard extends JComponent {
         private String key;
         private Key(String key){
             this.key = key;
-            setFont(new Font(font, Font.BOLD, (int) (size/2)));
-            setSize((int) size, (int) size);
             setText(key);
             setOpaque(false);
             setContentAreaFilled(false);
             setFocusPainted(false);
+            setBorderPainted(false);
             addActionListener(this);
         }
         public void resetSize(){
-            setFont(new Font(font, Font.BOLD, (int) (size/2)));
+            setFont(new Font(keyFont, Font.BOLD, (int) (size/2)));
             setSize((int) size, (int) size);
         }
         public void paint(Graphics g){
+            int sizeInt = (int) size;
+            int size8 = (int)(0.8 * size);
+            int size1 = (int)(0.1 * size);
+
+            // Draw key background
             Graphics2D g2 = (Graphics2D) g.create();
-            g2.setColor(Color.WHITE);
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.fillRoundRect((int)(size * 0.1), (int)(size * 0.1), (int)(size * 0.8), (int)(size * 0.8), (int)(size * 0.8), (int)(size * 0.8));
-            g2.setColor(Keyboard.this.getBackground());
-            g2.setFont(new Font(font, Font.BOLD, (int) (size * 0.4)));
-            g2.drawString(key, (int) (size*0.14), (int) (size * 0.64));
+            g2.setColor(Color.WHITE);
+            g2.fillRoundRect(size1, size1, size8, size8, size8, size8);
+
+            // Draw label
+            g2.setColor(keyColor);
+            g2.setFont(this.getFont());
+            FontMetrics fm = g2.getFontMetrics();
+            g2.drawString(key, (sizeInt - fm.stringWidth(key)) / 2, (sizeInt - size1 + fm.getAscent()) / 2);
             g2.dispose();
         }
         public void actionPerformed(ActionEvent e) {
-            listener.actionPerformed(new ActionEvent(e, e.getID(), "press " + key));
+            if (listener != null) {
+                listener.actionPerformed(new ActionEvent(this, e.getID(), "press " + key));
+            }
         }
     }
     public static void main(String[] args){
