bf3cbab85ff1e4b7988bddf04e14488db7f08a4f
williamwutq <williamwutq@gmail.com>
2025-05-12 17:36:18 -0700

    Added basic graphics to GamesPanel

--- Launcher/panel/GamesPanel.java
+++ Launcher/panel/GamesPanel.java
@@ -24,11 +24,39 @@
 
 package Launcher.panel;
 
+import GUI.GameEssentials;
+import Launcher.LaunchEssentials;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamesPanel extends JPanel {
+    double size;
     public GamesPanel(){
         super();
+        this.setOpaque(false);
+        this.setBackground(LaunchEssentials.launchBackgroundColor);
+        size = 1;
+    }
+
+    public void doLayout(){
+        size = Math.min(this.getBounds().width, this.getBounds().height) / 12.0;
+    }
+
+
+    public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D) g.create();
+        int sizeInt = (int) size;
+        int sizeHalf = (int) (size * 0.5);
+        int sizeOneHalf = (int) (size * 1.5);
+        int sizeThreeThird = (int) (size * 0.75);
+        g2.setColor(LaunchEssentials.launchTitlePanelBackgroundColor);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fillRoundRect(3+sizeHalf, 3+sizeHalf, getWidth()-6-sizeInt, getHeight()-6-sizeInt, sizeInt, sizeInt);
+        g2.setColor(this.getBackground());
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.fillRoundRect(3+sizeThreeThird, 3+sizeThreeThird, getWidth()-6-sizeOneHalf, getHeight()-6-sizeOneHalf, sizeHalf, sizeHalf);
+        g2.dispose();
+        paintChildren(g);
     }
 }
