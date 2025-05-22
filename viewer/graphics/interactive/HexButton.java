3931c9addc0a7a8bf6852094d7f10f016c08778d
williamwutq <williamwutq@gmail.com>
2025-05-21 19:36:07 -0700

    Add LICENSE, made public, removed test

--- viewer/graphics/interactive/HexButton.java
+++ viewer/graphics/interactive/HexButton.java
@@ -1,3 +1,27 @@
+/*
+  MIT License
+
+  Copyright (c) 2025 William Wu
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ */
+
 package viewer.graphics.interactive;
 
 import javax.swing.*;
@@ -32,7 +56,7 @@ import java.awt.geom.*;
  * @see JButton
  * @see Path2D.Double
  */
-abstract class HexButton extends JButton implements ActionListener, MouseListener {
+public abstract class HexButton extends JButton implements ActionListener, MouseListener {
     /** Border color of the HexButton, which is the button color normally and the button border color when hovered. */
     private static final Color borderColor = new Color(85, 85, 85);
     /** Constant value representing sin(60°), used in hexagon geometry calculations. */
@@ -271,22 +295,5 @@ abstract class HexButton extends JButton implements ActionListener, MouseListene
      */
     protected static double toAbsolute(int center, double radius, double relative){
         return center + relative * radius;
-    }
-
-    public static void main(String[] args){
-        viewer.Viewer.test(new HexButton(){
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Clicked");
-            }
-            protected Path2D.Double createCustomPath(int cx, int cy, double radius) {
-                radius /= 2;
-                Path2D.Double path = new Path2D.Double();
-                path.moveTo(cx - radius * (sinOf60 * 2 - 1 / sinOf60), cy + radius);
-                path.lineTo(cx - radius * (sinOf60 * 2 - 1 / sinOf60), cy - radius);
-                path.lineTo(cx + radius / sinOf60, cy);
-                path.closePath();
-                return path;
-            }
-        });
     }
 }
