575fc28ba5adaac5c065715b713b8a801dbda22d
williamwutq <williamwutq@gmail.com>
2025-05-22 22:51:44 -0700

    Cache hexagon and add javadoc

--- viewer/graphics/interactive/SpeedSlider.java
+++ viewer/graphics/interactive/SpeedSlider.java
@@ -41,13 +41,33 @@ public final class SpeedSlider extends JComponent implements MouseListener, Mous
     private int dragOffsetX = 0;
     private final Object lock = new Object();
     private Thread runnerThread;
+    private Path2D.Double cachedHexagon;
+    private int cachedHexagonHeight, cachedHexagonX;
+    /**
+     * Constructs a new {@code SpeedSlider} component, a custom graphical slider with a stylized hexagonal knob.
+     * <p>
+     * This constructor initializes the component, sets the background color,
+     * enables mouse interaction via {@link MouseListener} and {@link MouseMotionListener},
+     * and marks the component as non-opaque for custom painting.
+     */
     public SpeedSlider(){
+        this.cachedHexagon = null;
+        this.cachedHexagonHeight = -1;
+        this.cachedHexagonX = -1;
         this.setBackground(borderColor);
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
         this.setOpaque(false);
     }
 
+    /**
+     * Paints the slider component using {@link Graphics2D} with antialiasing for smooth visuals.
+     * <p>
+     * This method draws a rounded track and a hexagonal knob. It uses cached geometry for performance optimization.
+     *
+     * @param g the {@link Graphics} context in which to paint
+     * @see #getCachedHexagon()
+     */
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
@@ -57,24 +77,33 @@ public final class SpeedSlider extends JComponent implements MouseListener, Mous
         int eh = h * 3 / 8;
         int qh = h / 4;
         int hh = h / 2;
-        int knobX = (int)(knobPosition * (w - h)) + hh;
 
         g2.setStroke(new BasicStroke((float) hh / 8));
         g2.setColor(getBackground());
         g2.fillRoundRect(eh, eh, w - hh - qh, qh, qh, qh);
         g2.setColor(Color.WHITE);
-        g2.fill(createRoundedHexagon(knobX, hh, hh));
+        g2.fill(getCachedHexagon());
         g2.setColor(getBackground());
-        g2.draw(createRoundedHexagon(knobX, hh, hh));
+        g2.draw(getCachedHexagon());
         g2.dispose();
     }
 
+    /**
+     * Handles the mouse press event on the slider.
+     * <p>
+     * If the press occurs on the knob, dragging begins. If the press occurs elsewhere on the track,
+     * a thread is started to animate the knob smoothly toward the clicked position.
+     *
+     * @param e the {@link MouseEvent} representing the mouse press
+     * @see Thread
+     * @see #mouseReleased(MouseEvent)
+     */
     public void mousePressed(MouseEvent e) {
         int h = getHeight();
         int hh = h / 2;
         double halfH = h / 2.0;
         int knobX = hh + (int)(knobPosition * (getWidth()-h));
-        Path2D.Double knobBounds = createRoundedHexagon(knobX, hh, halfH);
+        Path2D.Double knobBounds = getCachedHexagon();
         if (knobBounds.contains(e.getPoint())) {
             // Start dragging
             dragging = true;
@@ -114,6 +143,14 @@ public final class SpeedSlider extends JComponent implements MouseListener, Mous
             runnerThread.start();
         }
     }
+    /**
+     * Handles the mouse release event.
+     * <p>
+     * This stops dragging and interrupts the {@code runnerThread}, if active.
+     *
+     * @param e the {@link MouseEvent} representing the mouse release
+     * @see #mousePressed(MouseEvent)
+     */
     public void mouseReleased(MouseEvent e) {
         dragging = false;
         if (runnerThread != null) {
@@ -121,10 +158,48 @@ public final class SpeedSlider extends JComponent implements MouseListener, Mous
             runnerThread = null;
         }
     }
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is currently unused but must be implemented due to the interface.
+     *
+     * @param e the {@link MouseEvent}
+     * @see java.awt.event.MouseListener
+     */
     public void mouseClicked(MouseEvent e) {}
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is currently unused but must be implemented due to the interface.
+     * @param e the {@link MouseEvent}
+     * @see java.awt.event.MouseListener
+     */
     public void mouseEntered(MouseEvent e) {}
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is currently unused but must be implemented due to the interface.
+     * @param e the {@link MouseEvent}
+     * @see java.awt.event.MouseListener
+     */
     public void mouseExited(MouseEvent e) {}
+    /**
+     * {@inheritDoc}
+     * <p>
+     * This method is currently unused but must be implemented due to the interface.
+     * @param e the {@link MouseEvent}
+     * @see java.awt.event.MouseListener
+     */
     public void mouseMoved(MouseEvent e) {}
+    /**
+     * Handles mouse dragging, used to manually move the knob when dragging is enabled.
+     * <p>
+     * Updates {@code knobPosition} based on the current mouse X position, constrained to [0, 1].
+     *
+     * @param e the {@link MouseEvent}
+     * @see #mousePressed(MouseEvent)
+     * @see #repaint()
+     */
     public void mouseDragged(MouseEvent e) {
         if (dragging) {
             double h = getHeight();
@@ -139,6 +214,29 @@ public final class SpeedSlider extends JComponent implements MouseListener, Mous
         }
     }
 
+    /**
+     * Retrieves the cached rounded hexagon shape corresponding to the current dimensions of the slider.
+     * <p>
+     * If the cached hexagon is null or the dimensions have changed since the last computation,
+     * a new rounded hexagon is generated using the current height of the slider.
+     * The hexagon is centered within the component and its size is determined by the minimum
+     * radius that fits within the height constraints.
+     *
+     * @return a {@link Path2D.Double} representing the rounded hexagon for the current size
+     * @see #createRoundedHexagon(int, int, double)
+     */
+    private Path2D.Double getCachedHexagon() {
+        int w = getWidth();
+        int h = getHeight();
+        int knobX = (int)(knobPosition * (w - h)) + h / 2;
+        if (cachedHexagon == null || h != cachedHexagonHeight || knobX != cachedHexagonX) {
+            double minRadius = Math.min(w / sinOf60 / 2.0, h / 2.0);
+            cachedHexagon = createRoundedHexagon(knobX, h / 2, minRadius);
+            cachedHexagonHeight = h;
+            cachedHexagonX = knobX;
+        }
+        return cachedHexagon;
+    }
     /**
      * Generate a rounded hexagon centered at the given coordinates.
      * <p>
