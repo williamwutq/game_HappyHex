24660d14b7d0dd2d36dfe3f65f9d36756b9361b1
williamwutq <williamwutq@gmail.com>
2025-06-11 20:48:59 +0800

    Implement basic autoplayimpl

--- python/autoplayimpl.py
+++ python/autoplayimpl.py
@@ -22,5 +22,37 @@
   SOFTWARE.
 """
 
-import comm
+from typing import Optional, List
+from comm import CommandProcessor
 import hex
+
+class MainProcessor(CommandProcessor):
+    def __init__(self):
+        self._callback: Optional[CommandProcessor] = None
+
+    def execute_command(self, command: str, args: List[str]) -> None:
+        if self._callback is not None:
+            if command == 'ping':
+                self._callback.execute('pong')
+            elif command == 'pong':
+                pass
+            elif command == 'hex':
+                if len(args) == 2:
+                    i = int(args[0])
+                    k = int(args[1])
+                    hex_coo = hex.Hex.hex(i, k)
+                    self._callback.execute(f'print {hex_coo}')
+                else: raise ValueError('Missing or extra coordinates for hex')
+            elif command == 'piece':
+                if len(args) == 1:
+                    p = hex.Piece.piece_from_byte(int(args[0]), -1)
+                    self._callback.execute(f'print {p}')
+                else: raise ValueError('Missing or extra argument for piece')
+            else: raise ValueError('Invalid command')
+
+    def get_callback_processor(self) -> Optional['CommandProcessor']:
+        return self._callback
+    def set_callback_processor(self, processor: Optional['CommandProcessor']) -> None:
+        if processor is self:
+            raise ValueError("Cannot add self as callback processor")
+        else: self._callback = processor
