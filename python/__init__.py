f81fcc3ac7b42db12e08864e139d1a0c235923a7
williamwutq <williamwutq@gmail.com>
2025-06-11 14:44:00 +0800

    Bump version

--- python/__init__.py
+++ python/__init__.py
@@ -2,5 +2,5 @@ import hex
 import hexio
 
 __all__ = ['hex', 'hexio', 'comm', '__version__', '__version_info__']
-__version__ = "1.3.0"
-__version_info__ = (1,3,0)
+__version__ = "1.3.3"
+__version_info__ = (1,3,3)
