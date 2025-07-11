b19cec22ef9aef1042a6404dc5cdec8f4e049eaa
williamwutq <williamwutq@gmail.com>
2025-07-11 15:15:25 +0800

    Add docs

--- GUI/DynamicColor.java
+++ GUI/DynamicColor.java
@@ -1,3 +1,27 @@
+/*
+  MIT License
+
+  Copyright (c) 2025 William Wu
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ */
+
 package GUI;
 
 import util.dynamic.ExtremalProperty;
@@ -12,8 +36,12 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * <p>
  * This class is thread-safe. All mutable shared state is accessed under synchronization or
  * via atomic variables.
+ * <p>
+ * Implements {@link ExtremalProperty} with {@link Color} as the value type.
  *
- * <p>Implements {@link ExtremalProperty} with {@link Color} as the value type.
+ * @author William Wu
+ * @version 1.4
+ * @since 1.4
  */
 public class DynamicColor implements ExtremalProperty<Color>, Runnable {
     private final Color defaultColor;
