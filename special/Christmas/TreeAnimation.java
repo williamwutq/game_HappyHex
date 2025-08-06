1d28ae0468d75eee10e63d9e3f6e42a097260207
williamwutq <williamwutq@gmail.com>
2025-08-06 16:08:02 -0700

    Add tree shape to TreeAnimation

--- special/Christmas/TreeAnimation.java
+++ special/Christmas/TreeAnimation.java
@@ -36,10 +36,57 @@ import GUI.animation.Animation;
 import java.awt.*;
 
 public class TreeAnimation extends Animation {
+    public static final double sinOf60 = Math.sqrt(3) / 2;
+    private static final int[][] colorArray = {
+            // R, G, B
+            {220, 151, 18},
+            {207, 140, 86},
+            {63, 189, 139},
+            {209, 104, 127},
+            {162, 118, 195},
+            {50, 154, 189},
+            {188, 185, 38},
+            {168, 107, 61},
+            {163, 84, 120},
+            {113, 168, 41},
+            {225, 80, 59},
+            {122, 121, 121}
+    };
+    private static final double[][] treePoints = {
+            // X, Y range: (-1, 1)
+            {0, -1},
+            {-0.6, -0.4},
+            {-0.3, -0.4},
+            {-0.9, 0.2},
+            {-0.6, 0.2},
+            {-1, 0.7},
+            {-0.2, 0.7},
+            {-0.2, 1},
+            {0.2, 1},
+            {0.2, 0.7},
+            {1, 0.7},
+            {0.6, 0.2},
+            {0.9, 0.2},
+            {0.3, -0.4},
+            {0.6, -0.4},
+    };
     private hex.Hex hex;
+    private final int colorIndex;
+    private static Color interpretColor(int index, int alpha){
+        // Custom color interpreter that is faster
+        if (index == -1 || index == 13){
+            // If block is empty
+            return new Color(68, 68, 68, alpha);
+        } else if (index < -1 || index >= colorArray.length) {
+            return new Color(122, 121, 121, alpha);
+        } else {
+            return new Color(colorArray[index][0], colorArray[index][1], colorArray[index][2], alpha);
+        }
+    }
     public TreeAnimation(hex.Block block) {
         super(2000, 1);
         hex = block.thisHex();
+        colorIndex = block.getColor();
     }
     public final void resetSize(){
         if(hex == null) {
@@ -68,5 +115,28 @@ public class TreeAnimation extends Animation {
     @Override
     protected void paintFrame(Graphics graphics, double progress) {
         resetSize();
+        double size = GUI.HexButton.getActiveSize();
+        double extended = 1.5;
+        int cooX = (int) Math.round(size * (sinOf60 + 0.5));
+        int cooY = (int) Math.round(size * extended);
+        int alpha = 255 - (int)(progress * progress * 255);
+        final Color treeColor = new Color(0, 128, 0, alpha);
+        final Color dominantColor = interpretColor(colorIndex, alpha);
+        final Color sideColor = interpretColor(11 - colorIndex, alpha);
+        // Note on special values:
+        // For normal colorIndex, it will return a normal color.
+        // For -1 (unoccupied), it will return gray.
+        // For -2 (default), it will return dark gray.
+        Graphics2D g2d = (Graphics2D) graphics.create();
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        int[] xPoints = new int[treePoints.length];
+        int[] yPoints = new int[treePoints.length];
+        for (int i = 0; i < treePoints.length; i++) {
+            xPoints[i] = (int) Math.round(cooX + size * treePoints[i][0] * sinOf60);
+            yPoints[i] = (int) Math.round(cooY + size * treePoints[i][1]);
+        }
+        g2d.setColor(treeColor);
+        g2d.fillPolygon(xPoints, yPoints, treePoints.length);
     }
 }
