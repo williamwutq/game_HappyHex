00e1eaad4e7b82b6b0e06e20029e179fb2d14def
williamwutq <williamwutq@gmail.com>
2025-09-06 17:42:20 -0700

    Upload autoplayimpl package

---
+++ python/tensorflowimpl/__init__.py
@@ -0,0 +1,8 @@
+'''
+A package for implementing autoplay algorithms using TensorFlow and Keras.
+This module is extracted from the HappyHex machine learning project, and is copyrighted by its contributors.
+For the original source, visit: github.com/williamwutq/hpyhexml
+
+This module provides functions to create prediction algorithms based on trained models.
+To create a prediction function, use the `autoplayimpl.create_model_predictor` function with the path to the model and the desired function name.
+'''
