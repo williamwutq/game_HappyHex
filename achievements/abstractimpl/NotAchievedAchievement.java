ea8073770d0462ea25685da01e7f72eb4f2afed9
williamwutq <williamwutq@gmail.com>
2025-09-14 00:02:04 -0700

    Add missing LICENSE and identity methods

--- achievements/abstractimpl/NotAchievedAchievement.java
+++ achievements/abstractimpl/NotAchievedAchievement.java
@@ -1,3 +1,27 @@
+/*
+  MIT License
+
+  Copyright (c) 2025 William Wu
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+ */
+
 package achievements.abstractimpl;
 
 import achievements.GameAchievementTemplate;
@@ -46,5 +70,31 @@ public class NotAchievedAchievement implements PhantomAchievementTemplate {
     @Override
     public boolean test(GameState state) {
         return !template.test(state);
+    }
+
+    /**
+     * {@inheritDoc}
+     * Two NotAchievedAchievement are considered equal if their wrapped templates are equal.
+     * This ensures that the equality check is based on the actual achievement logic rather than
+     * the wrapper itself. Note that the template does not equal to the NotAchievedAchievement wrapping it.
+     * @param obj the object to compare with
+     * @return {@code true} if the objects are equal, {@code false} otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof NotAchievedAchievement)) return false;
+        NotAchievedAchievement other = (NotAchievedAchievement) obj;
+        return template.equals(other.template);
+    }
+    /**
+     * The hash code is the bitwise NOT of the wrapped template's hash code.
+     * This ensures that the hash code is different from the original template,
+     * while still being consistent with equality.
+     * @return the hash code of this achievement
+     */
+    @Override
+    public int hashCode() {
+        return ~template.hashCode();
     }
 }
