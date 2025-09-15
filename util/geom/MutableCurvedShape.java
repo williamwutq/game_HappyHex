fa47006a2054ae71b1d8b5ad8ab5dd2b4488e42c
williamwutq <williamwutq@gmail.com>
2025-09-14 22:44:16 -0700

    Add MutableCurvedShape class for dynamic curved shape manipulation

---
+++ util/geom/MutableCurvedShape.java
@@ -0,0 +1,145 @@
+package util.geom;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a mutable curved shape defined by a series of points and control points for quadratic Bézier curves.
+ * Each point is defined by its (x, y) coordinates and a control point (controlX, controlY) that influences the curve
+ * between this point and the next point.
+ * The shape is closed by connecting the last point back to the first point using its control point.
+ * This class allows for dynamic modification of the shape by adding, removing, or changing points, and offers method
+ * to convert to an immutable {@link CurvedShape}.
+ */
+public class MutableCurvedShape {
+    private final List<double[]> points;
+    /**
+     * Constructs an empty MutableCurvedShape. This shape can be modified by adding, removing, or changing points.
+     */
+    public MutableCurvedShape(){
+        this.points = new ArrayList<>();
+    }
+    /**
+     * Adds a point to the MutableCurvedShape.
+     * @param x the x coordinate of the point
+     * @param y the y coordinate of the point
+     * @param controlX the x coordinate of the control point
+     * @param controlY the y coordinate of the control point
+     */
+    public void addPoint(double x, double y, double controlX, double controlY) {
+        points.add(new double[]{x, y, controlX, controlY});
+    }
+    /**
+     * Removes the last point from the MutableCurvedShape.
+     * If the shape is empty, this method does nothing.
+     */
+    public void removeLast() {
+        if (!points.isEmpty()) {
+            points.removeLast();
+        }
+    }
+    /**
+     * Removes the first point from the MutableCurvedShape.
+     * If the shape is empty, this method does nothing.
+     */
+    public void removeFirst() {
+        if (!points.isEmpty()) {
+            points.removeFirst();
+        }
+    }
+    /**
+     * Clears all points from the MutableCurvedShape.
+     */
+    public void clear() {
+        points.clear();
+    }
+    /**
+     * Sets the coordinates and control point of the specified point.
+     * @param index the index of the point to be set
+     * @param x the new x coordinate of the point
+     * @param y the new y coordinate of the point
+     * @param controlX the new x coordinate of the control point
+     * @param controlY the new y coordinate of the control point
+     */
+    public void setPoint(int index, double x, double y, double controlX, double controlY) {
+        if (index < 0 || index >= points.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + points.size());
+        }
+        points.set(index, new double[]{x, y, controlX, controlY});
+    }
+    /**
+     * Moves the specified point by the given delta values.
+     * @param index the index of the point to be moved
+     * @param deltaX the amount to move the point in the x direction
+     * @param deltaY the amount to move the point in the y direction
+     */
+    public void movePoint(int index, double deltaX, double deltaY) {
+        if (index < 0 || index >= points.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + points.size());
+        }
+        double[] point = points.get(index);
+        point[0] += deltaX;
+        point[1] += deltaY;
+    }
+    /**
+     * Moves the control point of the specified point by the given delta values.
+     * @param index the index of the point whose control point is to be moved
+     * @param deltaX the amount to move the control point in the x direction
+     * @param deltaY the amount to move the control point in the y direction
+     */
+    public void moveControl(int index, double deltaX, double deltaY) {
+        if (index < 0 || index >= points.size()) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + points.size());
+        }
+        double[] point = points.get(index);
+        point[2] += deltaX;
+        point[3] += deltaY;
+    }
+    /**
+     * Scales the entire MutableCurvedShape by the given factors in the x and y directions.
+     * @param scaleX the factor to scale in the x direction
+     * @param scaleY the factor to scale in the y direction
+     */
+    public void scale(double scaleX, double scaleY) {
+        for (double[] point : points) {
+            point[0] *= scaleX;
+            point[1] *= scaleY;
+            point[2] *= scaleX;
+            point[3] *= scaleY;
+        }
+    }
+    /**
+     * Scales the entire MutableCurvedShape uniformly by the given factor.
+     * @param scale the factor to scale in both the x and y directions
+     */
+    public void scale(double scale) {
+        scale(scale, scale);
+    }
+    /**
+     * Moves the entire MutableCurvedShape by the given delta values in the x and y directions.
+     * @param deltaX the amount to move in the x direction
+     * @param deltaY the amount to move in the y direction
+     */
+    public void move(double deltaX, double deltaY) {
+        for (double[] point : points) {
+            point[0] += deltaX;
+            point[1] += deltaY;
+            point[2] += deltaX;
+            point[3] += deltaY;
+        }
+    }
+    /**
+     * Returns the number of points in the MutableCurvedShape.
+     * @return the number of points
+     */
+    public int size() {
+        return points.size();
+    }
+    /**
+     * Converts the MutableCurvedShape to an immutable CurvedShape.
+     * @return a CurvedShape representing the current state of the MutableCurvedShape
+     */
+    public CurvedShape toCurvedShape() {
+        return new CurvedShape(points.toArray(new double[0][0]));
+    }
+}
