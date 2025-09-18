83cc4ae9b5a8187b94421b03c85093160219b5c1
williamwutq <williamwutq@gmail.com>
2025-09-17 22:32:22 -0700

    Add package-info.java

---
+++ achievements/icon/package-info.java
@@ -0,0 +1,25 @@
+
+/**
+ * This package contains classes and resources related to achievement icons.
+ * <p>
+ * It includes implementations for different types of achievement icons, such as empty icons,
+ * text-based icons, and shaped icons. Each icon type implements the {@link achievements.icon.AchievementIcon}
+ * interface, which defines the contract for rendering and managing achievement icons.
+ * <p>
+ * The package also provides a {@link achievements.icon.AchievementIconDeserializer utility class} for
+ * serializing and deserializing achievement icons to and from JSON format, facilitating easy storage
+ * and retrieval of icon data.
+ * <p>
+ * This package is dependent on the following libraries and packages:
+ * <ul>
+ *     <li>Java AWT (Abstract Window Toolkit) for graphical representation and manipulation of shapes and colors.</li>
+ *     <li>Java JSON Processing (javax.json) for handling JSON serialization and deserialization.</li>
+ *     <li>The {@code util.tuple.Pair} class providing basic tuple functionality,</li>
+ *     <li>The {@code util.geom.CurvedShape} class for representing complex shapes made out of quadratic Bézier curves.</li>
+ * </ul>
+ * @author William Wu
+ * @version 2.0
+ * @see achievements.icon.AchievementIcon
+ * @see achievements.icon.AchievementIconDeserializer
+ */
+package achievements.icon;
