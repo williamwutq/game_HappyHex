aa03ea1e4434a67cef712eebd3b819572d651348
williamwutq <williamwutq@gmail.com>
2025-10-01 19:22:13 -0700

    Add achievement related messages

--- Launcher/LauncherGUI.java
+++ Launcher/LauncherGUI.java
@@ -114,6 +114,19 @@ public class LauncherGUI {
                 "Try autoplay, I said",
                 "Games are no-brainer",
                 "What is the coolest feature?",
+                "Achievements are cool",
+                "Try to unlock all achievements",
+                "Have a goal, player",
+                "Trophies, what does it do?",
+                "Trophies, what are they?",
+                "Try to unlock all trophies",
+                "There's meanings behind games",
+                "Try to get all trophies",
+                "Could this game be beaten?",
+                "What is the meaning of life",
+                "How did we get to there",
+                "When theme change, icon stay",
+                "From welcome to ??????"
         };
         return hints[LaunchEssentials.getRandomIndex(hints.length)];
     }
